4e47dcbf2a874c5663cb0432ee166ecca2d1a235
savitris <11764984+savitris@users.noreply.github.com>
2023-08-29 17:23:01 +0200

    💄 default NextJS page + Button

--- packages/next-templates/src/app/page.tsx
+++ packages/next-templates/src/app/page.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { Button } from '@utrecht/component-library-react';
 import Image from 'next/image';
 import styles from './page.module.css';
 
@@ -9,6 +12,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className={styles['example-code']}>src/app/page.tsx</code>
         </p>
+        <Button appearance="primary-action-button">Hello!</Button>
         <div className={styles['example-description__illustration']}>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
