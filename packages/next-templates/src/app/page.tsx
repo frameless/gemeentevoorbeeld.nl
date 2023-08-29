70183ef2021199a0406e5adc01ea6f308523a82c
MeesoPost <100311542+MeesoPost@users.noreply.github.com>
2023-08-29 15:48:29 +0200

    ⚰️ this was test code so i removed it

--- packages/next-templates/src/app/page.tsx
+++ packages/next-templates/src/app/page.tsx
@@ -1,9 +1,5 @@
-'use client';
-
-import { Button } from '@utrecht/component-library-react';
 import Image from 'next/image';
 import styles from './page.module.css';
-
 export default function Home() {
   return (
     <main className={styles['example-main']}>
@@ -12,7 +8,6 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className={styles['example-code']}>src/app/page.tsx</code>
         </p>
-        <Button appearance="primary-action-button">Hello World!±</Button>
         <div className={styles['example-description__illustration']}>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
