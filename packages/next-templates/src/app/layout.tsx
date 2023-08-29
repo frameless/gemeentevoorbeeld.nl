e7a666844bbf6462218b5250f7ca7ef0418abf13
savitris <11764984+savitris@users.noreply.github.com>
2023-08-29 17:20:31 +0200

    🌐  lang nl body utrecht-theme

--- packages/next-templates/src/app/layout.tsx
+++ packages/next-templates/src/app/layout.tsx
@@ -11,8 +11,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="nl">
+      <body className="utrecht-theme">{children}</body>
     </html>
   );
 }
