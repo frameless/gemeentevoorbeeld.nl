7ba83777ccd6894f183b74fe514c3afba609a8ca
savitris <11764984+savitris@users.noreply.github.com>
2023-08-30 22:23:45 +0200

    🦺 💄 ♿️ form setup submit & reset buttons

--- packages/next-templates/src/app/savi/form/page.tsx
+++ packages/next-templates/src/app/savi/form/page.tsx
@@ -22,41 +22,45 @@ export default function Home() {
       <Paragraph>
         Staat op een bepaalde locatie de bakfiets of aanhanger er niet bij dan is deze in reparatie. <br />
         Voor de bakfiets geldt, zorg dat u deze niet te zwaar belaadt. Om het afvalstation te bereiken moet u namelijk
-        een heuveltje op. <br />
+        een heuveltje op.
       </Paragraph>
 
-      <FormField type="text">
-        <Paragraph>
-          <FormLabel htmlFor="1111">Aanhanger/bakfiets</FormLabel>
-        </Paragraph>
-        <Paragraph>
-          <Select size={10} id="1111" name="aanhanger bakfiets">
-            <SelectOption disabled selected value="--- Toon alle aanhangers/bakfietsen ---">
-              --- Toon alle aanhangers/bakfietsen ---
-            </SelectOption>
-            <SelectOption value="1000000001">Aanhanger 1Tractieweg</SelectOption>
-            <SelectOption value="1000000003">Aanhanger De Stits</SelectOption>
-            <SelectOption value="1000000002">Aanhanger Lunetten</SelectOption>
-            <SelectOption value="1000000004">Bakfiets De Stits</SelectOption>
-          </Select>
-        </Paragraph>
-      </FormField>
+      <form class="utrecht-form" method="GET" action="/savi/form/bevestiging/">
+        <FormField type="text">
+          <Paragraph>
+            <FormLabel htmlFor="1111">Aanhanger/bakfiets</FormLabel>
+          </Paragraph>
+          <Paragraph>
+            <Select size={10} id="1111" name="aanhanger bakfiets">
+              <SelectOption disabled selected value="--- Toon alle aanhangers/bakfietsen ---">
+                --- Toon alle aanhangers/bakfietsen ---
+              </SelectOption>
+              <SelectOption value="1000000001">Aanhanger 1Tractieweg</SelectOption>
+              <SelectOption value="1000000003">Aanhanger De Stits</SelectOption>
+              <SelectOption value="1000000002">Aanhanger Lunetten</SelectOption>
+              <SelectOption value="1000000004">Bakfiets De Stits</SelectOption>
+            </Select>
+          </Paragraph>
+        </FormField>
 
-      <FormField type="text">
-        <Paragraph>
-          <FormLabel htmlFor="2222">Datum</FormLabel>
-        </Paragraph>
-        <Paragraph>
-          <Textbox id="2222" name="datums beschikbaarheid" type="date" autoComplete="off" />
-        </Paragraph>
-      </FormField>
+        <FormField type="text">
+          <Paragraph>
+            <FormLabel htmlFor="2222">Datum</FormLabel>
+          </Paragraph>
+          <Paragraph>
+            <Textbox id="2222" name="datums beschikbaarheid" type="date" autoComplete="off" />
+          </Paragraph>
+        </FormField>
 
-      <ButtonGroup>
-        <Button appearance="primary-action-button">Bekijk schema</Button>
-        <Button appearance="secondary-action-button" type="reset" name="clear">
-          Wissen
-        </Button>
-      </ButtonGroup>
+        <ButtonGroup>
+          <Button appearance="primary-action-button" type="submit">
+            Bekijk schema
+          </Button>
+          <Button appearance="secondary-action-button" type="reset" name="clear">
+            Wissen
+          </Button>
+        </ButtonGroup>
+      </form>
     </Page>
   );
 }
