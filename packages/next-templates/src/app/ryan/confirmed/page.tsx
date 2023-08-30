7676914013c99ea5251ccb8432cabf383500487b
Ryan <ryan@frameless.io>
2023-08-30 18:26:39 +0200

    :construction: Added detail-page and confirmed page

---
+++ packages/next-templates/src/app/ryan/confirmed/page.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { Document, Heading1, Page, Paragraph } from '@utrecht/component-library-react';
+
+export default function Home() {
+  return (
+    <Document>
+      <Page>
+        <Heading1>Melding Ontvangen: Ontbreken van een Roze Eenhoornparkeerzone</Heading1>
+        <Paragraph>
+          Beste [Uw Naam],
+          <br />
+          <br />
+          Dank u wel voor uw creatieve bijdrage aan ons Absurde Meldpunt! We hebben uw melding over het ontbreken van
+          een roze eenhoornparkeerzone voor vliegende huisdieren ontvangen en kunnen gerust stellen dat uw verzoek zeker
+          in het rijk der verbeelding thuishoort.
+          <br />
+          <br />
+          Hoewel we misschien niet in staat zijn om echte roze eenhoornparkeerzones te creëren, waarderen we uw deelname
+          aan dit humoristische initiatief en uw bereidheid om een glimlach op ons gezicht te toveren.
+          <br />
+          <br />
+          Als u ooit serieuze kwesties wilt melden of contact wilt opnemen met ons gemeentelijke meldpunt voor meer
+          praktische zaken, bezoek dan onze officiële website voor de juiste contactinformatie.
+          <br />
+          <br />
+          Nogmaals bedankt voor uw fantasierijke bijdrage. We hopen dat u hebt genoten van deze unieke reis in de wereld
+          van verbeelding.
+          <br />
+          <br />
+          Met vriendelijke groet,
+          <br />
+          <br />
+          Het Team van het Gemeente Utrecht Absurde Meldpunt
+        </Paragraph>
+      </Page>
+    </Document>
+  );
+}
--- packages/next-templates/src/app/ryan/detail-page/page.tsx
+++ packages/next-templates/src/app/ryan/detail-page/page.tsx
@@ -1,24 +1,37 @@
 'use client';
 
-import {
-  Page,
-  Button,
-  Heading1,
-  Paragraph,
-  Textbox,
-  Textarea,
-  FormLabel,
-  FieldsetLegend,
-  Fieldset,
-  RadioButton,
-  FormField,
-  Document,
-} from '@utrecht/component-library-react';
-import Image from 'next/image';
-import styles from './page.module.css';
+import { ButtonLink, Document, Heading1, Page, Paragraph } from '@utrecht/component-library-react';
 
 export default function Home() {
-    return (
-        
-    );
+  return (
+    <Document>
+      <Page>
+        <Heading1>Melding ontbreken van een Roze Eenhoornparkeerzone</Heading1>
+        <Paragraph>
+          We begrijpen dat sommige van onze fantasierijke bewoners zich zorgen maken over het ontbreken van een
+          essentiële faciliteit in onze gemeente: een parkeerzone speciaal voor roze eenhoorns. We erkennen de unieke
+          behoeften van deze betoverende wezens en hun metgezellen, de vliegende huisdieren. Terwijl dit meldpunt vooral
+          dient voor lichtvoetige absurditeit, willen we u graag een plek bieden om uw creativiteit te uiten.
+          <br />
+          <br />
+          Ons doel is om een glimlach op uw gezicht te toveren en uw gevoel voor avontuur aan te wakkeren. Dus voel u
+          vrij om uw gedachten, visuele kunstwerken en eenhoornverhalen met ons te delen. Vergeet niet dat terwijl we
+          misschien niet in staat zijn om een echte roze eenhoornparkeerzone te creëren, we altijd genieten van uw
+          deelname aan dit humoristische initiatief.
+        </Paragraph>
+        <Paragraph>
+          <b>Ga naar het Meldingsformulier:</b>
+        </Paragraph>
+        <ButtonLink appearance="primary-action-button" href="./form">
+          Maak melding
+        </ButtonLink>
+        <Paragraph>
+          <i>
+            Dank u voor uw bijdrage aan ons Absurde Meldpunt. We hopen dat u hebt genoten van deze unieke reis in de
+            wereld van verbeelding.
+          </i>
+        </Paragraph>
+      </Page>
+    </Document>
+  );
 }
