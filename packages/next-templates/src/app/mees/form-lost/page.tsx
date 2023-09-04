d2a546f77394ef0cd013e7cfaaa1eeee7912fede
MeesoPost <100311542+MeesoPost@users.noreply.github.com>
2023-09-04 10:58:26 +0200

    🎨 replaced the unnecessary UUID's with appropriate ID's

--- packages/next-templates/src/app/mees/form-lost/page.tsx
+++ packages/next-templates/src/app/mees/form-lost/page.tsx
@@ -22,76 +22,70 @@ export default function Home() {
         <Heading1>Maak een melding van uw kwijtgeraakte sok</Heading1>
         <FormField type="text">
           <Paragraph>
-            <FormLabel htmlFor="3a43bbe9-635c-46c2-86cd-b0aa63855598">Voornaam</FormLabel>
+            <FormLabel htmlFor="Voornaam">Voornaam</FormLabel>
           </Paragraph>
           <Paragraph>
-            <Textbox id="3a43bbe9-635c-46c2-86cd-b0aa63855598" name="message" type="text" />
+            <Textbox id="Voornaam" name="message" type="text" />
           </Paragraph>
         </FormField>
         <FormField type="text">
           <Paragraph>
-            <FormLabel htmlFor="c0b1972a-467c-11ee-be56-0242ac120002">Achternaam</FormLabel>
+            <FormLabel htmlFor="Achternaam">Achternaam</FormLabel>
           </Paragraph>
           <Paragraph>
-            <Textbox id="c0b1972a-467c-11ee-be56-0242ac120002" name="message" type="text" />
+            <Textbox id="Achternaam" name="message" type="text" />
           </Paragraph>
         </FormField>
         <FormField type="text">
           <Paragraph>
-            <FormLabel htmlFor="aae98c3f-64ab-4127-a123-a0e932cff8ca">Telefoon</FormLabel>
+            <FormLabel htmlFor="Telefoon">Telefoon</FormLabel>
           </Paragraph>
           <Paragraph>
-            <Textbox id="aae98c3f-64ab-4127-a123-a0e932cff8ca" name="message" type="text" />
+            <Textbox id="Telefoon" name="message" type="text" />
           </Paragraph>
         </FormField>
         <FormField type="text">
           <Paragraph>
-            <FormLabel htmlFor="d16b6417-6b16-482b-b032-c5a4e003e449">E-mail</FormLabel>
+            <FormLabel htmlFor="Email">E-mail</FormLabel>
           </Paragraph>
           <Paragraph>
-            <Textbox id="d16b6417-6b16-482b-b032-c5a4e003e449" name="message" type="text" />
+            <Textbox id="Email" name="message" type="text" />
           </Paragraph>
         </FormField>
         <FormField type="text">
           <Paragraph>
-            <FormLabel htmlFor="3661edb0-b50b-429d-8567-84a9c81f2562">Beschrijving sok en/of sokken</FormLabel>
+            <FormLabel htmlFor="Beschrijving">Beschrijving sok en/of sokken</FormLabel>
           </Paragraph>
-          <FormFieldDescription id="66c5c73a-6e76-484c-b691-cca7fa1309b8">
+          <FormFieldDescription id="Beschrijving">
             Geef zo goed mogelijk de kenmerken van de sok aan,<Paragraph></Paragraph> zodat we hem zo snel mogelijk met
             zijn vriendje kunnen herenigen.
           </FormFieldDescription>
           <Paragraph>
-            <Textarea
-              aria-describedby="66c5c73a-6e76-484c-b691-cca7fa1309b8"
-              id="3661edb0-b50b-429d-8567-84a9c81f2562"
-              name="message"
-            />
+            <Textarea aria-describedby="Beschrijving" name="message" />
           </Paragraph>
         </FormField>
         <FormField type="text">
           <Paragraph>
-            <FormLabel htmlFor="46a02385-905e-40fe-8f2e-97499814bce6">Waar ben je je sok verloren?</FormLabel>
+            <FormLabel htmlFor="Waar-gevonden">Waar ben je je sok verloren?</FormLabel>
           </Paragraph>
           <Paragraph>
-            <Textarea id="46a02385-905e-40fe-8f2e-97499814bce6" name="message" />
+            <Textarea id="Waar-gevonden" name="message" />
           </Paragraph>
           <FormField type="text">
             <Paragraph>
-              <FormLabel htmlFor="4e1f98e4-2749-4088-84a0-4e3841cf8869">Plaatsnaam</FormLabel>
+              <FormLabel htmlFor="Plaatsnaam">Plaatsnaam</FormLabel>
             </Paragraph>
             <Paragraph>
-              <Textbox id="4e1f98e4-2749-4088-84a0-4e3841cf8869" name="message" type="text" />
+              <Textbox id="Plaatsnaam" name="message" type="text" />
             </Paragraph>
           </FormField>
         </FormField>
         <FormField type="text">
           <Paragraph>
-            <FormLabel htmlFor="5d88f6e7-3e62-48c2-bf0c-5756e3b63efa">
-              Wanneer heb je je sok voor het laatst gezien?
-            </FormLabel>
+            <FormLabel htmlFor="Laatst-gezien">Wanneer heb je je sok voor het laatst gezien?</FormLabel>
           </Paragraph>
           <Paragraph>
-            <Textbox id="5d88f6e7-3e62-48c2-bf0c-5756e3b63efa" name="lost-date" type="date" />
+            <Textbox id="Laatst-gezien" name="lost-date" type="date" />
           </Paragraph>
         </FormField>
         <ButtonGroup>
