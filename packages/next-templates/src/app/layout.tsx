4207daa8761a4459283102f867658309e35df4d7
Rids <74712581+Ridsko@users.noreply.github.com>
2023-09-05 10:29:07 +0200

    ✨Voorbeeld Thema ingeladen

--- packages/next-templates/src/app/layout.tsx
+++ packages/next-templates/src/app/layout.tsx
@@ -3,7 +3,7 @@ import '@utrecht/component-library-css';
 import '@utrecht/design-tokens/dist/index.css';
 import type { Metadata } from 'next';
 import '@utrecht/component-library-css';
-import '@utrecht/design-tokens/dist/index.css';
+import '@nl-design-system-unstable/voorbeeld-design-tokens/dist/index.css';
 import React from 'react';
 
 export const metadata: Metadata = {
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="nl">
-      <body className="utrecht-theme">{children}</body>
+      <body className="voorbeeld-theme">{children}</body>
     </html>
   );
 }
