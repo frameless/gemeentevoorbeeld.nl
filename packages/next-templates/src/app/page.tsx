80b353846d0b41c760cefc7e0597dc691c101bc5
Rids <74712581+Ridsko@users.noreply.github.com>
2023-09-05 13:44:08 +0200

    🍱 Added svg logo header

--- packages/next-templates/src/app/page.tsx
+++ packages/next-templates/src/app/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 import { Button, Link, PageHeader, Textbox } from '@utrecht/component-library-react';
+import Logo from './styling/voorbeeld.svg';
 
 export default function Home() {
   return (
     <main className="voorbeeld-theme">
       <PageHeader>
         {/* SVGR gebruiken ipv react-component*/}
-
+        <Logo />
         <Link>Mijn omgeving</Link>
         <Link>Contact</Link>
         <Textbox placeholder="Bijvoorbeeld zwembad of grofvuil" />
