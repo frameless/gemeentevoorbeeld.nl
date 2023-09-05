cadb402c772f552d3f003e1a74dea86006598fad
Ryan <ryan@frameless.io>
2023-09-05 15:31:11 +0200

    :pencil2: Changed classnames

--- packages/next-templates/src/app/layout.tsx
+++ packages/next-templates/src/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="nl">
-      <body className="example-theme">{children}</body>
+      <body className="voorbeeld-theme">{children}</body>
     </html>
   );
 }
