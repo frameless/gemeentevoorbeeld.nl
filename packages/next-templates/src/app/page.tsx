5db970f7ccb1af5f4422c50a3e1225114805c262
Rids <74712581+Ridsko@users.noreply.github.com>
2023-09-05 10:37:07 +0200

    Comment

---
+++ packages/next-templates/src/app/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+import { Button, Link, PageHeader, Textbox } from '@utrecht/component-library-react';
+import Logo from './voorbeeld';
+
+export default function Home() {
+  return (
+    <main className="voorbeeld-theme">
+      <PageHeader>
+        {/* SVGR gebruiken ipv react-component*/}
+        <Logo />
+
+        <Link>Mijn omgeving</Link>
+        <Link>Contact</Link>
+        <Textbox placeholder="Bijvoorbeeld zwembad of grofvuil" />
+        <Button>Zoeken</Button>
+      </PageHeader>
+    </main>
+  );
+}
