e73a20af26f6cf4269dd0e7145040a2d90df1d11
Ryan <ryan@frameless.io>
2023-09-05 15:22:52 +0200

    :pencil2: Changed typos

--- packages/next-templates/src/app/layout.tsx
+++ packages/next-templates/src/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="nl">
-      <body className="voorbeeld-theme">{children}</body>
+      <body className="example-theme">{children}</body>
     </html>
   );
 }
