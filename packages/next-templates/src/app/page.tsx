c026407770d7ffe6b0063880054cb4fc29ae7887
Rids <74712581+Ridsko@users.noreply.github.com>
2023-09-06 10:05:38 +0200

    🎨 header verder gemaakt

--- packages/next-templates/src/app/page.tsx
+++ packages/next-templates/src/app/page.tsx
@@ -1,17 +1,26 @@
 'use client';
 import { Button, Link, PageHeader, Textbox } from '@utrecht/component-library-react';
 import Logo from './styling/assets/voorbeeld.svg';
+import './styling/component-styles/header.css';
 
 export default function Home() {
   return (
     <main className="voorbeeld-theme">
-      <PageHeader>
+      <PageHeader className="header--home-page">
         {/* SVGR gebruiken ipv react-component*/}
-        <Logo />
-        <Link>Mijn omgeving</Link>
-        <Link>Contact</Link>
-        <Textbox placeholder="Bijvoorbeeld zwembad of grofvuil" />
-        <Button>Zoeken</Button>
+        <div className="logo">
+          <Logo />
+        </div>
+        <div className="header-items">
+          <div className="nav-links">
+            <Link>Mijn omgeving</Link>
+            <Link>Contact</Link>
+          </div>
+          <div className="search-box">
+            <Textbox placeholder="Bijvoorbeeld zwembad of grofvuil" />
+            <Button>Zoeken</Button>
+          </div>
+        </div>
       </PageHeader>
     </main>
   );
