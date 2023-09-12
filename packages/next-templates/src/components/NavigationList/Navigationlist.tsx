a0bf8c8395c5757db0e76720762f99414716d86a
MeesoPost <100311542+MeesoPost@users.noreply.github.com>
2023-09-12 10:10:31 +0200

    🎨  PropsWithChilderen already is in HTMLAtributes, and classname doesnt need to be in brackets

--- packages/next-templates/src/components/NavigationList/Navigationlist.tsx
+++ packages/next-templates/src/components/NavigationList/Navigationlist.tsx
@@ -1,9 +1,11 @@
 import React, { HTMLAttributes, PropsWithChildren } from 'react';
 import './index.css';
 
-interface NavigationListProps extends PropsWithChildren<HTMLAttributes<HTMLUListElement>> {}
-export const NavigationList = ({ children }: NavigationListProps) => (
-  <ul className="voorbeeld-navigation-list">{children}</ul>
+interface NavigationListProps extends HTMLAttributes<HTMLUListElement> {}
+export const NavigationList = ({ children, ...props }: NavigationListProps) => (
+  <ul className={'voorbeeld-navigation-list'} {...props}>
+    {children}
+  </ul>
 );
 
 NavigationList.displayName = 'NavigationList';
