528f3855b0da2461f25959c494a468366a320e72
MeesoPost <100311542+MeesoPost@users.noreply.github.com>
2023-09-12 11:54:34 +0200

    🚚 css files changed and moved

--- packages/next-templates/src/components/NavigationList/Navigationlist.tsx
+++ packages/next-templates/src/components/NavigationList/Navigationlist.tsx
@@ -1,9 +1,9 @@
 import React, { HTMLAttributes, PropsWithChildren } from 'react';
-import './index.css';
+import './Navigationlist.css';
 
 interface NavigationListProps extends HTMLAttributes<HTMLUListElement> {}
 export const NavigationList = ({ children, ...props }: NavigationListProps) => (
-  <ul className={'voorbeeld-navigation-list'} {...props}>
+  <ul className={'voorbeeld-navigation-list'} style={{ listStyleType: 'none' }} {...props}>
     {children}
   </ul>
 );
