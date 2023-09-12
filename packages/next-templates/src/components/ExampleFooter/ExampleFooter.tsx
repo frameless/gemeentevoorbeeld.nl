808fd04014915db0e5931302c10d50500433fd5a
MeesoPost <100311542+MeesoPost@users.noreply.github.com>
2023-09-12 13:37:54 +0200

    ✨ made the Example-Footer component

---
+++ packages/next-templates/src/components/ExampleFooter/ExampleFooter.tsx
@@ -0,0 +1,38 @@
+import Logo from '../../app/styling/assets/voorbeeld-footer.svg';
+import { NavigationList } from '../NavigationList/Navigationlist';
+import { NavigationListItem } from '../NavigationList/NavigationListIItem';
+import { Heading5, PageFooter, Paragraph, UnorderedList, UnorderedListItem } from '@utrecht/component-library-react';
+import React, { HTMLAttributes } from 'react';
+import './footer.css';
+
+interface ExampleFooterProps extends HTMLAttributes<HTMLDivElement> {}
+export const ExampleFooter = ({ ...props }: ExampleFooterProps) => (
+  <PageFooter>
+    <div className="example-link-list-container" {...props}>
+      <Logo className="example--footer-logo" />
+      <NavigationList>
+        <Heading5>Heading</Heading5>
+        <NavigationListItem className="utrecht-link-list__item" href="example.com" label="example" />
+        <NavigationListItem className="utrecht-link-list__item" href="example.com" label="example" />
+        <NavigationListItem className="utrecht-link-list__item" href="example.com" label="example" />
+        <NavigationListItem className="utrecht-link-list__item" href="example.com" label="example" />
+      </NavigationList>
+      <NavigationList>
+        <Heading5>Heading</Heading5>
+        <NavigationListItem className="utrecht-link-list__item" href="example.com" label="example" />
+        <NavigationListItem className="utrecht-link-list__item" href="example.com" label="example" />
+        <NavigationListItem className="utrecht-link-list__item" href="example.com" label="example" />
+        <NavigationListItem className="utrecht-link-list__item" href="example.com" label="example" />
+      </NavigationList>
+      <UnorderedList>
+        <Heading5>Heading</Heading5>
+        <UnorderedListItem className="utrecht-link-list__item" />
+        <Paragraph className="utrecht-link-list__item">
+          Lorem ipsum dolor sit amet, consectetur ad * isicing elit, sed do eiusmod *
+        </Paragraph>
+      </UnorderedList>
+    </div>
+  </PageFooter>
+);
+
+ExampleFooter.displayName = 'ExampleFooter';
