55a17d30a03e0076c8fd1d513088d383b7a20271
MeesoPost <100311542+MeesoPost@users.noreply.github.com>
2023-09-12 15:41:05 +0200

    🔥 removed unnecessary PropsWithChilderen from NaigationList component

--- packages/next-templates/src/components/NavigationList/NavigationListIItem.tsx
+++ packages/next-templates/src/components/NavigationList/NavigationListIItem.tsx
@@ -1,11 +1,12 @@
 import { Link } from '@utrecht/component-library-react';
-import React, { HTMLAttributes, PropsWithChildren } from 'react';
+import React, { HTMLAttributes } from 'react';
 import './Navigationlist.css';
 
-interface NavigationListItemProps extends PropsWithChildren<HTMLAttributes<HTMLElement>> {
+interface NavigationListItemProps extends HTMLAttributes<HTMLElement> {
   href: string;
   label: string;
 }
+
 export const NavigationListItem = ({ href, label }: NavigationListItemProps) => (
   <li className="voorbeeld-navigation-list__item">
     <Link href={href}>{label}</Link>
