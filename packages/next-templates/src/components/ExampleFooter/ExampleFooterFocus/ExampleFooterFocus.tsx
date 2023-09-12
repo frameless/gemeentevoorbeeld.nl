fc3d3bdc4f8c4c12784bbda78a4856ed223f2291
MeesoPost <100311542+MeesoPost@users.noreply.github.com>
2023-09-12 15:36:36 +0200

    ✨ ExampleFooterFocus component is made and ready

---
+++ packages/next-templates/src/components/ExampleFooter/ExampleFooterFocus/ExampleFooterFocus.tsx
@@ -0,0 +1,21 @@
+import Logo from '../../app/styling/assets/voorbeeld-footer.svg';
+import { Heading5, PageFooter, Paragraph, UnorderedList, UnorderedListItem } from '@utrecht/component-library-react';
+import React, { HTMLAttributes } from 'react';
+import './footer.css';
+
+interface ExampleFooterFocusProps extends HTMLAttributes<HTMLDivElement> {}
+export const ExampleFooterFocus = ({ ...props }: ExampleFooterFocusProps) => (
+  <PageFooter>
+    <div className="example-link-list-container" {...props}>
+      <Logo className="example--footer-logo" />
+      <UnorderedList>
+        <Heading5>Heading</Heading5>
+        <Paragraph className="utrecht-link-list__item">
+          Lorem ipsum dolor sit amet, consectetur ad * isicing elit, sed do eiusmod *
+        </Paragraph>
+      </UnorderedList>
+    </div>
+  </PageFooter>
+);
+
+ExampleFooterFocus.displayName = 'ExampleFooter';
