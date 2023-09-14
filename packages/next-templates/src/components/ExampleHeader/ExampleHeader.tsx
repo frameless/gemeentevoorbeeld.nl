3441a0828043c9235ccc581f68c6d438e9c52a6e
scar055 <rowanvanlaar@gmail.com>
2023-09-14 14:14:22 +0200

    🔥 navigation weggehaald bij header

--- packages/next-templates/src/components/ExampleHeader/ExampleHeader.tsx
+++ packages/next-templates/src/components/ExampleHeader/ExampleHeader.tsx
@@ -7,34 +7,25 @@ import './header.css';
 
 interface ExampleHeaderProps extends HTMLAttributes<HTMLDivElement> {}
 export const ExampleHeader = ({ ...props }: ExampleHeaderProps) => (
-  <Page>
-    <PageHeader className="example--header-home-page">
-      <div className="example--logo">
-        <Logo />
-      </div>
-      <div className="example--header-items">
-        <div className="example--header-links-container">
-          <Link className="example--header-links">
-            <User className="example--header-user-icon" /> Mijn omgeving
-          </Link>
-          <Link className="example--header-links">Contact</Link>
-        </div>
-        <div className="example--search-box">
-          <Textbox placeholder="Bijvoorbeeld zwembad of grofvuil" />
-          <Button appearance="secondary-action-button">
-            <Search /> Zoeken
-          </Button>
-        </div>
-      </div>
-    </PageHeader>
-    <div className="example--header-navbar">
-      <Link className="example--header-navbar-active">Home</Link>
-      <Link>About</Link>
-      <Link>Services</Link>
-      <Link>Portfolio</Link>
-      <Link>Contact</Link>
+  <PageHeader className="example--header-home-page">
+    <div className="example--logo">
+      <Logo />
     </div>
-  </Page>
+    <div className="example--header-items">
+      <div className="example--header-links-container">
+        <Link className="example--header-links">
+          <User className="example--header-user-icon" /> Mijn omgeving
+        </Link>
+        <Link className="example--header-links">Contact</Link>
+      </div>
+      <div className="example--search-box">
+        <Textbox placeholder="Bijvoorbeeld zwembad of grofvuil" />
+        <Button appearance="secondary-action-button">
+          <Search /> Zoeken
+        </Button>
+      </div>
+    </div>
+  </PageHeader>
 );
 
 ExampleHeader.displayName = 'ExampleHeader';
