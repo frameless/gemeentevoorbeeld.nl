c4b30cba3d26209b88f0c56d5c8be5cf26c49c68
MeesoPost <100311542+MeesoPost@users.noreply.github.com>
2023-09-15 16:03:19 +0200

    ✨ everything is now in a document giving the header the right font

--- packages/next-templates/src/app/layout.tsx
+++ packages/next-templates/src/app/layout.tsx
@@ -3,7 +3,7 @@ import '@utrecht/design-tokens/dist/index.css';
 import type { Metadata } from 'next';
 import '@nl-design-system-unstable/voorbeeld-design-tokens/dist/index.css';
 import './globals.css';
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 import './font';
 
 export const metadata: Metadata = {
@@ -11,11 +11,13 @@ export const metadata: Metadata = {
   description: 'Demo van Utrecht formulieren voor verschillende services',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: PropsWithChildren<{}>) {
   return (
     <html lang="nl">
       {/* <body className="voorbeeld-theme">{children}</body> */}
-      <body className="voorbeeld-theme">{children}</body>
+      <body className="voorbeeld-theme">
+        <div className="utrecht-document">{children}</div>
+      </body>
     </html>
   );
 }
