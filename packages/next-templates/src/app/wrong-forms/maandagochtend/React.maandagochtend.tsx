55a3faa71b5a813f6b940f51ed0c32078088eb61
AnoniemeBoebe <NejmAynaou@outlook.com>
2023-10-11 14:23:05 +0200

    📦️ Hookform installed en used in form
    📦️Installed react hookform

--- packages/next-templates/src/app/wrong-forms/maandagochtend/React.maandagochtend.tsx
+++ packages/next-templates/src/app/wrong-forms/maandagochtend/React.maandagochtend.tsx
@@ -1,45 +1,92 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { ErrorMessage } from '@hookform/error-message';
+import React, { useState } from 'react';
 
-interface FormInputs {
-  multipleErrorInput: string;
-}
-
-export default function App() {
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm<FormInputs>({
-    criteriaMode: 'all',
+export default function FormValidation() {
+  const [formData, setFormData] = useState({
+    naam: '',
+    straat: '',
+    huisnummer: '',
+    postcode: '',
+    woonplaats: '',
+    telefoonnummer: '',
+    mailadress: '',
   });
-  const onSubmit = (data: FormInputs) => console.log(data);
 
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        {...register('multipleErrorInput', {
-          required: 'This is required.',
-          pattern: {
-            value: /d+/,
-            message: 'This input is number only.',
-          },
-          maxLength: {
-            value: 10,
-            message: 'This input exceed maxLength.',
-          },
-        })}
-      />
-      <ErrorMessage
-        errors={errors}
-        name="multipleErrorInput"
-        render={({ messages }) =>
-          messages && Object.entries(messages).map(([type, message]) => <p key={type}>{message}</p>)
-        }
-      />
+  const [errors, setErrors] = useState({
+    naam: '',
+    straat: '',
+    huisnummer: '',
+    postcode: '',
+    woonplaats: '',
+    telefoonnummer: '',
+    mailadress: '',
+  });
 
-      <input type="submit" />
-    </form>
-  );
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newErrors = { ...errors };
+
+    if (formData.naam.trim() === '') {
+      newErrors.naam = 'Vul een valide naam in';
+    } else {
+      newErrors.naam = '';
+    }
+
+    if (formData.straat.trim() === '') {
+      newErrors.straat = 'Vul geldige straat in';
+    } else {
+      newErrors.straat = '';
+    }
+
+    if (formData.huisnummer.trim() === '') {
+      newErrors.huisnummer = 'Kies een huisnummer';
+    } else {
+      newErrors.huisnummer = '';
+    }
+
+    if (formData.postcode.trim() === '') {
+      newErrors.postcode = 'Invalid';
+    } else {
+      newErrors.postcode = '';
+    }
+
+    if (formData.woonplaats.trim() === '') {
+      newErrors.woonplaats = 'Invoer klopt niet! Pas aan!';
+    } else {
+      newErrors.woonplaats = '';
+    }
+
+    if (formData.telefoonnummer.trim() === '') {
+      newErrors.telefoonnummer = 'Error code 112';
+    } else {
+      newErrors.telefoonnummer = '';
+    }
+
+    if (formData.mailadress.trim() === '') {
+      newErrors.mailadress = 'Ongeldig';
+    } else {
+      newErrors.mailadress = '';
+    }
+
+    if (!Object.values(newErrors).some((error) => error !== '')) {
+      setErrors(newErrors);
+    }
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <label>
+          Naam:
+          <input type="text" name="naam" value={formData.naam} onChange={handleInputChange} />
+          <div className="error">{errors.naam}</div>
+        </label>
+
+        <button type="submit">Versturen</button>
+      </form>
+    );
+  };
 }
