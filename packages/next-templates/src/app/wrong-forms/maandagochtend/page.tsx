df634d93b4c73399427d66496b228fbdfa051f2a
AnoniemeBoebe <NejmAynaou@outlook.com>
2023-10-11 14:23:05 +0200

    📦️ Hookform installed en used in form
    📦️Installed react hookform

--- packages/next-templates/src/app/wrong-forms/maandagochtend/page.tsx
+++ packages/next-templates/src/app/wrong-forms/maandagochtend/page.tsx
@@ -1,7 +1,8 @@
 'use client';
+import React, { useState } from 'react';
+
 import {
   ButtonGroup,
-  ButtonLink,
   Document,
   FormField,
   FormFieldDescription,
@@ -14,13 +15,85 @@ import {
   RadioButton,
   Textarea,
   Textbox,
+  Button,
 } from '@utrecht/component-library-react';
+
 import { ExampleFooter } from '@/components/ExampleFooter/ExampleFooter';
 import { ExampleHeaderFunnel } from '@/components/ExampleHeader/ExampleHeaderFunnel/ExampleHeaderFunnel';
 import '../../styling/css/maandagochtend.css';
-import './React.maandagochtend.tsx';
+import { Form } from 'react-hook-form';
+
+
+type FormErrors = {
+  selectedReason?: string;
+  name?: string;
+  street?: string;
+  houseNumber?: string;
+  postcode?: string;
+  woonplaats?: string;
+  email?: string;
+};
 
 export default function Home() {
+  const [selectedReason, setSelectedReason] = useState<string>('');
+  const [additionalInfo, setAdditionalInfo] = useState<string>('');
+  const [name, setName] = useState<string>('');
+  const [street, setStreet] = useState<string>('');
+  const [houseNumber, setHouseNumber] = useState<string>('');
+  const [postcode, setPostcode] = useState<string>('');
+  const [woonplaats, setWoonplaats] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [telefoonnummer, setTelefoonnummer] = useState<string>('');
+  const [formErrors, setFormErrors] = useState<FormErrors>({});
+
+  const isValidEmail = (email: string): boolean => {
+    return true;
+  };
+
+  const validateForm = () => {
+    const errors: FormErrors = {};
+
+    if (!selectedReason) {
+      errors.selectedReason = 'Selecteer een reden is verplicht';
+    }
+
+    if (!name) {
+      errors.name = 'Naam is verplicht';
+    }
+
+    if (!street) {
+      errors.street = 'Straat is verplicht';
+    }
+
+    if (!houseNumber) {
+      errors.houseNumber = 'Huisnummer is verplicht';
+    }
+
+    if (!postcode) {
+      errors.postcode = 'Postcode is verplicht';
+    }
+
+    if (!woonplaats) {
+      errors.woonplaats = 'Woonplaats is verplicht';
+    }
+
+    if (!email) {
+      errors.email = 'E-mailadres is verplicht';
+    } else if (!isValidEmail(email)) {
+      errors.email = 'Ongeldig e-mailadres';
+    }
+
+    setFormErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (validateForm()) {
+      alert('Formulier verstuurd!');
+    }
+  };
+
   return (
     <Document>
       <ExampleHeaderFunnel />
@@ -29,71 +102,95 @@ export default function Home() {
           <Heading1>Bezwaar indienen tegen maandagochtend</Heading1>
           <Paragraph>Vul alle velden in. Als een veld optioneel is, dan staat dit erbij.</Paragraph>
           <Heading2>Waarom maakt u bezwaar?</Heading2>
-          <Paragraph>Kies een catogorie die bij uw melding past.</Paragraph>
+          <Paragraph>Kies een categorie die bij uw melding past.</Paragraph>
+          <Form method="post">
           <FormField>
             <FormLabel className="example-foute-form-label">Selecteer een reden</FormLabel>
+            <RadioButton
+              onChange={(e) => setSelectedReason(e.target.value)}
+              checked={selectedReason === 'Nog een weekend modus'}
+              value="Nog een weekend modus"
+            />
+            Nog een weekend modus
           </FormField>
           <FormField>
-            <RadioButton /> Nog een weekend modus
+            <RadioButton
+              onChange={(e) => setSelectedReason(e.target.value)}
+              checked={selectedReason === 'Weekend te kort'}
+              value="Weekend te kort"
+            />
+            Weekend te kort
           </FormField>
           <FormField>
-            <RadioButton /> Weekend te kort
+            <RadioButton
+              onChange={(e) => setSelectedReason(e.target.value)}
+              checked={selectedReason === 'Vermoeidheid'}
+              value="Vermoeidheid"
+            />
+            Vermoeidheid
           </FormField>
           <FormField>
-            <RadioButton /> Vermoeidheid
+            <RadioButton
+              onChange={(e) => setSelectedReason(e.target.value)}
+              checked={selectedReason === 'Puberteit'}
+              value="Puberteit"
+            />
+            Puberteit
           </FormField>
           <FormField>
-            <RadioButton /> Puberteit
-          </FormField>
-          <FormField>
-            <RadioButton /> Ajax heeft verloren
+            <RadioButton
+              onChange={(e) => setSelectedReason(e.target.value)}
+              checked={selectedReason === 'Ajax heeft verloren'}
+              value="Ajax heeft verloren"
+            />
+            Ajax heeft verloren
           </FormField>
           <FormField>
             <FormLabel className="example-foute-form-label">Aanvullende Onderbouwing</FormLabel>
             <FormFieldDescription>
-              Onderbouw de reden die u heeft opgegeven en beschrijf wat er eventueel wat er aan gedaan kan worden.
+              Onderbouw de reden die u heeft opgegeven en beschrijf wat er eventueel aan gedaan kan worden.
             </FormFieldDescription>
-            <Textarea></Textarea>
+            <Textarea onChange={(e) => setAdditionalInfo(e.target.value)} value={additionalInfo} />
           </FormField>
           <Heading2>Uw gegevens</Heading2>
           <FormField>
             <FormLabel className="example-foute-form-label">Naam</FormLabel>
-            <Textbox />
+            <Textbox onChange={(e) => setName(e.target.value)} value={name} />
           </FormField>
           <FormField>
             <FormLabel className="example-foute-form-label">Straat</FormLabel>
-            <Textbox />
+            <Textbox onChange={(e) => setStreet(e.target.value)} value={street} />
           </FormField>
           <FormField>
             <FormLabel className="example-foute-form-label">Huisnummer</FormLabel>
-            <Textbox />
+            <Textbox onChange={(e) => setHouseNumber(e.target.value)} value={houseNumber} />
           </FormField>
           <FormField>
-            <FormLabel className="example-foute-form-label">Toevoeging(Optioneel)</FormLabel>
+            <FormLabel className="example-foute-form-label">Toevoeging (Optioneel)</FormLabel>
             <Textbox />
           </FormField>
           <FormField>
             <FormLabel className="example-foute-form-label">Postcode</FormLabel>
-            <Textbox />
+            <Textbox onChange={(e) => setPostcode(e.target.value)} value={postcode} />
           </FormField>
           <FormField>
             <FormLabel className="example-foute-form-label">Woonplaats</FormLabel>
-            <Textbox />
+            <Textbox onChange={(e) => setWoonplaats(e.target.value)} value={woonplaats} />
           </FormField>
           <FormField>
             <FormLabel className="example-foute-form-label">E-mailadres</FormLabel>
-            <Textbox />
+            <Textbox onChange={(e) => setEmail(e.target.value)} value={email} />
           </FormField>
           <FormField>
             <FormLabel className="example-foute-form-label">Telefoonnummer</FormLabel>
-            <Textbox />
+            <Textbox onChange={(e) => setTelefoonnummer(e.target.value)} value={telefoonnummer} />
           </FormField>
-
           <ButtonGroup>
-            <ButtonLink href="#" appearance="primary-action-button">
+            <Button appearance="primary-action-button" type="submit" >
               Versturen
-            </ButtonLink>
+            </Button>
           </ButtonGroup>
+          </Form>
         </PageContent>
       </Page>
       <ExampleFooter />
