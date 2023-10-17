a152a414f555f4e47a46271c48b075b9338e7b5d
scar055 <rowanvanlaar@gmail.com>
2023-10-17 11:37:41 +0200

    🔥 div container deleted

--- packages/next-templates/src/components/ExampleNavigation/ExampleNavigation.tsx
+++ packages/next-templates/src/components/ExampleNavigation/ExampleNavigation.tsx
@@ -9,60 +9,58 @@ import { UtrechtFlexWrapFallback } from '@utrecht/web-component-library-react';
 interface ExampleNavigationProps extends HTMLAttributes<HTMLDivElement> {}
 export const ExampleNavigation = ({ ...props }: ExampleNavigationProps) => (
   <UtrechtFlexWrapFallback flexTarget="nav">
-    <div>
-      <div id="nav" className="example--header-navbar" {...props}>
-        <Link className="example--header-navbar-links" href="#">
+    <div id="nav" className="example--header-navbar" {...props}>
+      <Link className="example--header-navbar-links" href="#">
+        Home
+      </Link>
+      <Link className="example--header-navbar-links" href="#">
+        About
+      </Link>
+      <Link className="example--header-navbar-links" href="#">
+        Services
+      </Link>
+      <Link className="example--header-navbar-links" href="#">
+        Portfolio
+      </Link>
+      <Link className="example--header-navbar-links" href="#">
+        Contact
+      </Link>
+    </div>
+    <Drawer data-modal="true" id="popovermenu" className="example--header-popover-menu">
+      <div className="example--header-popover-container-button">
+        <PopOverButton
+          appearance="subtle-button"
+          className="example--popover-close"
+          popoverTarget="popovermenu"
+          popoverTargetAction="hide"
+        >
+          <Xbold className="example--header-xlogo" />
+          Sluiten
+        </PopOverButton>
+      </div>
+      <div className="example--header-popover-container-links">
+        <Link href="#" className="example--header-links">
           Home
         </Link>
-        <Link className="example--header-navbar-links" href="#">
+        <Link href="#" className="example--header-links">
           About
         </Link>
-        <Link className="example--header-navbar-links" href="#">
+        <Link href="#" className="example--header-links">
           Services
         </Link>
-        <Link className="example--header-navbar-links" href="#">
+        <Link href="#" className="example--header-links">
           Portfolio
         </Link>
-        <Link className="example--header-navbar-links" href="#">
+      </div>
+      <div className="example--header-user">
+        <Link href="#" className="example--header-links">
           Contact
         </Link>
+        <Link href="#" className="example--header-links">
+          <User className="example--header-user-icon" /> Mijn omgeving
+        </Link>
       </div>
-      <Drawer data-modal="true" id="popovermenu" className="example--header-popover-menu">
-        <div className="example--header-popover-container-button">
-          <PopOverButton
-            appearance="subtle-button"
-            className="example--popover-close"
-            popoverTarget="popovermenu"
-            popoverTargetAction="hide"
-          >
-            <Xbold className="example--header-xlogo" />
-            Sluiten
-          </PopOverButton>
-        </div>
-        <div className="example--header-popover-container-links">
-          <Link href="#" className="example--header-links">
-            Home
-          </Link>
-          <Link href="#" className="example--header-links">
-            About
-          </Link>
-          <Link href="#" className="example--header-links">
-            Services
-          </Link>
-          <Link href="#" className="example--header-links">
-            Portfolio
-          </Link>
-        </div>
-        <div className="example--header-user">
-          <Link href="#" className="example--header-links">
-            Contact
-          </Link>
-          <Link href="#" className="example--header-links">
-            <User className="example--header-user-icon" /> Mijn omgeving
-          </Link>
-        </div>
-      </Drawer>
-    </div>
+    </Drawer>
   </UtrechtFlexWrapFallback>
 );
 
