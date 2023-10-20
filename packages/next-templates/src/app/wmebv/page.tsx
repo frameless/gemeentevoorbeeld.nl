1daa3ce60118da30b6eac6e699034584810e5738
MeesoPost <100311542+MeesoPost@users.noreply.github.com>
2023-10-20 16:21:58 +0200

    ✨ added wmebv page

---
+++ packages/next-templates/src/app/wmebv/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { UtrechtHeading1 } from '@utrecht/web-component-library-react';
+import {} from '@utrecht/component-library-react';
+
+export default function home() {
+  return (
+    <>
+      <UtrechtHeading1>Voorbeeld</UtrechtHeading1>
+      <UtrechtHeading1>Voorbeeld</UtrechtHeading1>
+    </>
+  );
+}
