c031fa185752bfb670f6c321c5bb7b85d177b666
MeesoPost <100311542+MeesoPost@users.noreply.github.com>
2023-10-25 14:05:41 +0200

    🚑 build fix

--- packages/next-templates/src/app/wmebv/Home/page.tsx
+++ packages/next-templates/src/app/wmebv/Home/page.tsx
@@ -13,7 +13,7 @@ import { ExampleHeaderWmebv } from '@/components/ExampleHeader/wmebv/ExampleHead
 import { ExampleFooter } from '@/components/ExampleFooter/ExampleFooter';
 import { ExampleNavigation } from '@/components/ExampleNavigation/ExampleNavigation';
 import { BreadcrumbNav, BreadcrumbNavLink } from '@utrecht/component-library-react';
-import ChevronRight from '../../styling/assets/chevronRight.svg';
+// import ChevronRight from '../../styling/assets/chevronRight.svg';
 import '@/app/styling/css/wmebv.css';
 
 export default function home() {
@@ -23,10 +23,10 @@ export default function home() {
       <ExampleNavigation></ExampleNavigation>
       <BreadcrumbNav label="Kruimelpad">
         <BreadcrumbNavLink className="voorbeeld-breadcrumb-text-decoration" href="" index={0} rel="home">
-          Home <ChevronRight className="voorbeeld-chevron-right-space" />
+          {/* Home <ChevronRight className="voorbeeld-chevron-right-space" /> */}
         </BreadcrumbNavLink>
         <BreadcrumbNavLink className="voorbeeld-breadcrumb-text-decoration" href="" index={1}>
-          Contact <ChevronRight className="voorbeeld-chevron-right-space" />
+          {/* Contact <ChevronRight className="voorbeeld-chevron-right-space" /> */}
         </BreadcrumbNavLink>
         <BreadcrumbNavLink className="voorbeeld-breadcrumb-text-decoration" href="" index={2} rel="up">
           Ik heb een vraag
