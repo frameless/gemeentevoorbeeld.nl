92d7ea0d37400dfae77cfa31629856279312ce04
MeesoPost <100311542+MeesoPost@users.noreply.github.com>
2023-10-27 15:39:23 +0200

    🎨 updated import path

--- packages/next-templates/src/app/wmebv/page.tsx
+++ packages/next-templates/src/app/wmebv/page.tsx
@@ -15,7 +15,7 @@ import { ExampleHeaderWmebv } from '@/components/ExampleHeader/wmebv/ExampleHead
 import { ExampleFooter } from '@/components/ExampleFooter/ExampleFooter';
 import { ExampleNavigation } from '@/components/ExampleNavigation/ExampleNavigation';
 import { BreadcrumbNav, BreadcrumbNavLink } from '@utrecht/component-library-react';
-import ChevronRight from '../styling/assets/chevronRight.svg';
+import ChevronRight from '../../app/styling/assets/chevronRight.svg';
 import '@/app/styling/css/wmebv.css';
 
 export default function home() {
