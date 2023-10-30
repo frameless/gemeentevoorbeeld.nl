d9f5c695f7c3c98e356d98343fe9e664b0aee881
Robbert Broersma <git@robbertbroersma.nl>
2023-10-30 14:10:13 +0100

    fix groningen theme

--- packages/next-templates/src/app/layout.tsx
+++ packages/next-templates/src/app/layout.tsx
@@ -1,22 +1,22 @@
-import '@utrecht/component-library-css';
-import '@utrecht/design-tokens/dist/index.css';
-import type { Metadata } from 'next';
-import '@nl-design-system-unstable/voorbeeld-design-tokens/dist/index.css';
-import '@nl-design-system-unstable/groningen-design-tokens/dist/index.css';
-import './globals.css';
-import React, { PropsWithChildren } from 'react';
-import './font';
+import "@utrecht/component-library-css";
+import "@utrecht/design-tokens/dist/index.css";
+import type { Metadata } from "next";
+import "@nl-design-system-unstable/voorbeeld-design-tokens/dist/index.css";
+import "@/app/styling/css/groningen-theme.css";
+import "./globals.css";
+import React, { PropsWithChildren } from "react";
+import "./font";
 
 export const metadata: Metadata = {
-  title: 'Demo Utrecht formulieren',
-  description: 'Demo van Utrecht formulieren voor verschillende services',
+  title: "Demo Utrecht formulieren",
+  description: "Demo van Utrecht formulieren voor verschillende services",
 };
 
 export default function RootLayout({ children }: PropsWithChildren<{}>) {
   return (
     <html lang="nl">
       {/* <body className="voorbeeld-theme">{children}</body> */}
-      <body>
+      <body className="groningen-theme">
         <div className="utrecht-document">{children}</div>
       </body>
     </html>
