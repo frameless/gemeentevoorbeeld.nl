adc64921f907ea3d981251fee9d1688f77a6d50b
scar055 <rowanvanlaar@gmail.com>
2023-10-31 13:42:04 +0100

    💄 2e drawer gemaakt

--- packages/next-templates/src/app/drawer-scroll-list/erfgoed/page.tsx
+++ packages/next-templates/src/app/drawer-scroll-list/erfgoed/page.tsx
@@ -11,7 +11,6 @@ import {
   Paragraph,
 } from '@utrecht/component-library-react';
 import '../styles/drawer-scroll-list.css';
-
 import BackArrow from '../pijltje-rechts.svg';
 import Kruisje from '../kruisje.svg';
 
@@ -27,69 +26,107 @@ export default function Home() {
         </header>
         <ul className="voorbeeld--unorderedlist-lijst">
           <li>
-            <Link id="voorbeeld-link-duitse_bommenwerper" href="#voorbeeld--button-drawer">
+            <Link id="duitse_bommenwerper" href="#voorbeeld--button-drawer">
               Duitse bommenwerper
             </Link>
           </li>
           <li>
-            <Link>Werk aan de werf</Link>
+            <Link id="werf" href="#voorbeeld--button-drawer2">
+              Werk aan de werf
+            </Link>
           </li>
           <li>
-            <Link>Bouw en sloop van kasteel Vredenburg </Link>
+            <Link id="vredebrug" href="#">
+              Bouw en sloop van kasteel Vredenburg{' '}
+            </Link>
           </li>
           <li>
-            <Link>Ceciliaklooster</Link>
+            <Link id="cecilianklooster" href="#">
+              Ceciliaklooster
+            </Link>
           </li>
           <li>
-            <Link>Middeleeuwse stadsmuur</Link>
+            <Link id="middeleeuse-stadsmuur" href="#">
+              Middeleeuwse stadsmuur
+            </Link>
           </li>
           <li>
-            <Link>Oude tempel in Rijnvliet?</Link>
+            <Link id="rijmvliet" href="#">
+              Oude tempel in Rijnvliet?
+            </Link>
           </li>
           <li>
-            <Link>Dorp aan de rivier</Link>
+            <Link id="dorp-aan-rivier" href="#">
+              Dorp aan de rivier
+            </Link>
           </li>
           <li>
-            <Link>Romeinse wachttorens</Link>
+            <Link id="romeinse-wachttoren" href="#">
+              Romeinse wachttorens
+            </Link>
           </li>
           <li>
-            <Link>Kerkje van Blauwkapel</Link>
+            <Link id="blauwkapel" href="#">
+              Kerkje van Blauwkapel
+            </Link>
           </li>
           <li>
-            <Link>De Utrechtse werven</Link>
+            <Link id="utrechtse-werven" href="#">
+              De Utrechtse werven
+            </Link>
           </li>
           <li>
-            <Link>Domplein</Link>
+            <Link id="domplein" href="#">
+              Domplein
+            </Link>
           </li>
           <li>
-            <Link>Haardkuil uit de oude steentijd</Link>
+            <Link id="haardkuil-steentijd" href="#">
+              Haardkuil uit de oude steentijd
+            </Link>
           </li>
           <li>
-            <Link>Janskerkhof</Link>
+            <Link id="janskerkhof" href="#">
+              Janskerkhof
+            </Link>
           </li>
           <li>
-            <Link>Bijlhouwerstoren en Sterrenburg</Link>
+            <Link id="sterrenburg" href="#">
+              Bijlhouwerstoren en Sterrenburg
+            </Link>
           </li>
           <li>
-            <Link>Natuur op de forten</Link>
+            <Link id="natuur" href="#">
+              Natuur op de forten
+            </Link>
           </li>
           <li>
-            <Link>Buurtoren</Link>
+            <Link id="buurtoren" href="#">
+              Buurtoren
+            </Link>
           </li>
           <li>
-            <Link>Rondom de Plompetorengracht</Link>
+            <Link id="plompetorengracht" href="#">
+              Rondom de Plompetorengracht
+            </Link>
           </li>
           <li>
-            <Link>Herderplein in buurt Halve Maan</Link>
+            <Link id="herderplein" href="#">
+              Herderplein in buurt Halve Maan
+            </Link>
           </li>
           <li>
-            <Link>Rondom de Nieuwe Kamp</Link>
+            <Link href="#" id="kamp">
+              Rondom de Nieuwe Kamp
+            </Link>
           </li>
           <li>
-            <Link> Kanunniken te paard?</Link>
+            <Link href="#" id="kanunniken">
+              Kanunniken te paard?
+            </Link>
           </li>
           <li>
-            <Link id="paulusabdij" href="#voorbeeld--button-drawer">
+            <Link href="#" id="paulusabdij">
               Paulusabdij
             </Link>
           </li>
@@ -98,8 +135,7 @@ export default function Home() {
 
       <Drawer id="voorbeeld--button-drawer">
         <header>
-          <Heading3>Duitse bommenwerper</Heading3>
-          <ButtonLink id="voorbeeld-button-update" href="#voorbeeld-link-duitse_bommenwerper">
+          <ButtonLink id="voorbeeld-button-update" href="#duitse_bommenwerper">
             <BackArrow />
           </ButtonLink>
           <Button>
@@ -135,6 +171,50 @@ export default function Home() {
             <div>
               <dt>Tags</dt>
               <dd>Oorlog en verdediging</dd>
+            </div>
+          </DataList>
+        </div>
+      </Drawer>
+
+      <Drawer id="voorbeeld--button-drawer2">
+        <header>
+          <ButtonLink id="voorbeeld-button-update" href="#werf">
+            <BackArrow />
+          </ButtonLink>
+          <Button>
+            <Kruisje />
+          </Button>
+        </header>
+        <div className="content">
+          <Image
+            alt="Constructie bij de werf"
+            src="/nl-design-system-templates/packages/next-templates/src/app/drawer-scroll-list/erfgoed/duitsebommer.jpg"
+          />
+        </div>
+        <div className="details">
+          <Heading3>Werk aan de werf</Heading3>
+          <Paragraph>
+            Utrecht staat bekend om zijn werven en werfkelders. De eigenaren van de grachtenpanden moesten deze vroeger
+            onderhouden. In de 20e eeuw raakten de werven in verval. Toen nam de gemeente Utrecht het eigendom van de
+            werfmuren en kades over. Verschillende restauratierondes volgden.
+          </Paragraph>
+        </div>
+        <div className="links">
+          <Link href="https://www.cssscript.com/easy-sliding-drawer/">SVGLees het verhaal op de site SVG2 </Link>
+        </div>
+        <div className="details">
+          <DataList>
+            <div>
+              <dt>Periode</dt>
+              <dd>Moderne tijd</dd>
+            </div>
+            <div>
+              <dt>Thema</dt>
+              <dd>Gebouwen</dd>
+            </div>
+            <div>
+              <dt>Tags</dt>
+              <dd>Woningbouw</dd>
             </div>
           </DataList>
         </div>
