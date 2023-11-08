c74861c9b3213477b8bc1da51e2d4729de2d5a39
scar055 <rowanvanlaar@gmail.com>
2023-11-08 14:16:26 +0100

    🚚 renamed classnames

--- packages/next-templates/src/app/card-component/card/page.tsx
+++ packages/next-templates/src/app/card-component/card/page.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import { Heading2, Page, Paragraph } from '@utrecht/component-library-react';
+import { Heading2, Paragraph } from '@utrecht/component-library-react';
 import clsx from 'clsx';
 import Image from 'next/image';
 import Link from 'next/link';
 import React, { FC, HTMLAttributes } from 'react';
 import './index.style.css';
 
-interface CardProps extends Omit<HTMLAttributes<HTMLDivElement>, 'children'> {
+interface CardListItemProps extends Omit<HTMLAttributes<HTMLLIElement>, 'children'> {
   title: string;
   description: string;
   image: {
@@ -19,32 +19,38 @@ interface CardProps extends Omit<HTMLAttributes<HTMLDivElement>, 'children'> {
   };
 }
 
-export const Card: FC<CardProps> = ({ description, title, image: { url = '', alt }, link: { href }, ...props }) => {
+export const CardListItem: FC<CardListItemProps> = ({
+  description,
+  title,
+  image: { url = '', alt },
+  link: { href },
+  ...props
+}) => {
   const linkRef = React.useRef<HTMLAnchorElement>(null);
 
   return (
-    <div {...props} className={clsx('utrecht-card', props.className)} onClick={() => linkRef.current?.click()}>
-      {url && <Image src={url} alt={alt} className={'utrecht-card__image'} width={312} height={200} />}
-      <div className={'utrecht-card__content'}>
-        <Heading2 className="utrecht-card__title">
+    <li {...props} className={clsx('utrecht-card-list-item', props.className)} onClick={() => linkRef.current?.click()}>
+      {url && <Image src={url} alt={alt} className={'utrecht-card-list-item__image'} width={312} height={200} />}
+      <div className={'utrecht-card-list-item__content'}>
+        <Heading2 className="utrecht-card-list-item__title">
           <Link ref={linkRef} href={href} className={'utrecht-link utrecht-link--html-a'}>
             {title}
           </Link>
         </Heading2>
-        <Paragraph className="utrecht-card__body">{description}</Paragraph>
+        <Paragraph className="utrecht-card-list-item__body">{description}</Paragraph>
       </div>
-    </div>
+    </li>
   );
 };
 export default function Home() {
   return (
-    <Page>
-      <Card
+    <ul>
+      <CardListItem
         description="test test test test test test test test"
         title="test"
         image={{ url: '/business_corgi.jpeg', alt: 'test' }}
         link={{ href: '#' }}
       />
-    </Page>
+    </ul>
   );
 }
