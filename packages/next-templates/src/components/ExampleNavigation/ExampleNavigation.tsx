dc69488987762ee8818a2e8dffc5a2e0f2975f33
Robbert Broersma <git@robbertbroersma.nl>
2023-11-08 16:30:47 +0100

    ✨ NavigationBar component

--- packages/next-templates/src/components/ExampleNavigation/ExampleNavigation.tsx
+++ packages/next-templates/src/components/ExampleNavigation/ExampleNavigation.tsx
@@ -1,26 +1,55 @@
-import { Link } from '@utrecht/component-library-react';
-import React, { HTMLAttributes } from 'react';
-import './Navigation.css';
+import React, { HTMLAttributes, PropsWithChildren } from 'react';
+import { Navigation, NavigationLink, NavigationLinkButton, NavigationLinkList } from './Navigation';
+import './Navigation.scss';
 
-interface ExampleNavigationProps extends HTMLAttributes<HTMLDivElement> {}
-export const ExampleNavigation = ({ ...props }: ExampleNavigationProps) => (
-  <div className="example--header-navbar">
-    <Link className="example--header-navbar-links" href="#">
-      Home
-    </Link>
-    <Link className="example--header-navbar-links" href="#">
-      About
-    </Link>
-    <Link className="example--header-navbar-links" href="#">
-      Services
-    </Link>
-    <Link className="example--header-navbar-links" href="#">
-      Portfolio
-    </Link>
-    <Link className="example--header-navbar-links" href="#">
-      Contact
-    </Link>
-  </div>
+interface NavigationProps extends HTMLAttributes<HTMLDivElement> {
+  /* TODO: `center` should probably not be a boolean property, but an enum such as `appearance` */
+  center?: boolean;
+  links?: { href: string; label: string }[];
+}
+
+export const ExampleNavigation = ({
+  links = [
+    {
+      label: 'Home',
+      href: '#',
+    },
+    {
+      label: 'About',
+      href: '#',
+    },
+    {
+      label: 'Services',
+      href: '#',
+    },
+    {
+      label: 'Portfolio',
+      href: '#',
+    },
+    {
+      label: 'Contact',
+      href: '#',
+    },
+    {
+      label: 'Uitloggen',
+      href: '',
+    },
+  ],
+  center = true,
+}: NavigationProps) => (
+  <Navigation center={center}>
+    <NavigationLinkList center={center}>
+      {links.map(({ label, href }) =>
+        href ? (
+          <NavigationLink center={center} href={href}>
+            {label}
+          </NavigationLink>
+        ) : (
+          <NavigationLinkButton className="example-navbar__link">{label}</NavigationLinkButton>
+        ),
+      )}
+    </NavigationLinkList>
+  </Navigation>
 );
 
-ExampleNavigation.displayName = 'ExampleHeader';
+ExampleNavigation.displayName = 'ExampleNavigation';
