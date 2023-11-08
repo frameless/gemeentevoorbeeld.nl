cd0432ee3e118c3c785f7079add8579d4f364640
AnoniemeBoebe <NejmAynaou@outlook.com>
2023-11-08 11:11:06 +0100

    🚨 lint-fix

---
+++ packages/next-templates/src/app/wmebv/Niet-Inloggen/stap-4.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import {
+  UtrechtArticle,
+  UtrechtButton,
+  UtrechtForm,
+  UtrechtFormFieldDescription,
+  UtrechtFormFieldTextarea,
+  UtrechtHeading,
+  UtrechtLink,
+  UtrechtPage,
+  UtrechtPageContent,
+  UtrechtParagraph,
+} from '@utrecht/web-component-library-react';
+import { ExampleHeaderWmebv } from '@/components/ExampleHeader/wmebv/ExampleHeaderWmebv';
+import { ExampleFooter } from '@/components/ExampleFooter/ExampleFooter';
+import { ExampleNavigation } from '@/components/ExampleNavigation/ExampleNavigation';
+import { FormField, FormLabel, UnorderedList } from '@utrecht/component-library-react';
+import '@/app/styling/css/wmebv.css';
+
+export default function home() {
+  return (
+    <UtrechtPage>
+      <UtrechtPageContent>
+        <UtrechtArticle></UtrechtArticle>
+      </UtrechtPageContent>
+    </UtrechtPage>
+  );
+}
