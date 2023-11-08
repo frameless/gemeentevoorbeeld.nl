2ccacfaa8ba23a1c7503291fc738cb6ee718a9f2
scar055 <rowanvanlaar@gmail.com>
2023-11-08 15:53:00 +0100

    🎨 card list item alle items die nodig zijn added

--- packages/next-templates/src/app/card-component/card/page.tsx
+++ packages/next-templates/src/app/card-component/card/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Heading2, Paragraph } from '@utrecht/component-library-react';
+import { Heading, Paragraph } from '@utrecht/component-library-react';
 import clsx from 'clsx';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -8,8 +8,9 @@ import React, { FC, HTMLAttributes } from 'react';
 import './index.style.css';
 
 interface CardListItemProps extends Omit<HTMLAttributes<HTMLLIElement>, 'children'> {
+  headerlevel: number;
   title: string;
-  description: string;
+  children: string;
   image: {
     url: string;
     alt: string;
@@ -20,7 +21,8 @@ interface CardListItemProps extends Omit<HTMLAttributes<HTMLLIElement>, 'childre
 }
 
 export const CardListItem: FC<CardListItemProps> = ({
-  description,
+  headerlevel,
+  children,
   title,
   image: { url = '', alt },
   link: { href },
@@ -32,12 +34,17 @@ export const CardListItem: FC<CardListItemProps> = ({
     <li {...props} className={clsx('utrecht-card-list-item', props.className)} onClick={() => linkRef.current?.click()}>
       {url && <Image src={url} alt={alt} className={'utrecht-card-list-item__image'} width={312} height={200} />}
       <div className={'utrecht-card-list-item__content'}>
-        <Heading2 className="utrecht-card-list-item__title">
-          <Link ref={linkRef} href={href} className={'utrecht-link utrecht-link--html-a'}>
-            {title}
-          </Link>
-        </Heading2>
-        <Paragraph className="utrecht-card-list-item__body">{description}</Paragraph>
+        <article>
+          <hgroup>
+            <Heading level={headerlevel} className="utrecht-card-list-item__title">
+              <a ref={linkRef} href={href} className={'utrecht-link utrecht-link--html-a'}>
+                {title}
+              </a>
+            </Heading>
+            <p className="preheading">preheading</p>
+          </hgroup>
+          <p className="utrecht-card-list-item__body">{children}</p>
+        </article>
       </div>
     </li>
   );
@@ -46,11 +53,13 @@ export default function Home() {
   return (
     <ul>
       <CardListItem
-        description="test test test test test test test test"
+        headerlevel={2}
         title="test"
         image={{ url: '/business_corgi.jpeg', alt: 'test' }}
         link={{ href: '#' }}
-      />
+      >
+        test test test test test test test test test test test test test test test test
+      </CardListItem>
     </ul>
   );
 }
