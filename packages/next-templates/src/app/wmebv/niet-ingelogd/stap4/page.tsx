5179684f1646e09223bbacba4d0f57bfd5da37a2
AnoniemeBoebe <NejmAynaou@outlook.com>
2023-11-08 15:22:20 +0100

    ✨ Made the Stap 4 van 4 form

---
+++ packages/next-templates/src/app/wmebv/niet-ingelogd/stap4/page.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import {
+  UtrechtArticle,
+  UtrechtButtonLink,
+  UtrechtHeading1,
+  UtrechtPage,
+  UtrechtPageContent,
+  UtrechtParagraph,
+  UtrechtLink,
+  UtrechtSpotlightSection,
+} from '@utrecht/web-component-library-react';
+import { ExampleHeaderWmebv } from '@/components/ExampleHeader/wmebv/ExampleHeaderWmebv';
+import { ExampleFooter } from '@/components/ExampleFooter/ExampleFooter';
+import { ExampleNavigation } from '@/components/ExampleNavigation/ExampleNavigation';
+import { UnorderedList, UnorderedListItem } from '@utrecht/component-library-react';
+import Printer from '@/app/styling/assets/printer-icon.svg';
+import FileText from '@/app/styling/assets/filetext-icon.svg';
+import CircleCheck from '../../../styling/assets/circleCheck.svg';
+import '@/app/styling/css/wmebv.css';
+
+export default function home() {
+  return (
+    <UtrechtPage>
+      <ExampleHeaderWmebv></ExampleHeaderWmebv>
+      <ExampleNavigation></ExampleNavigation>
+      <UtrechtPageContent className="voorbeeld-page-content-flex">
+        <UtrechtArticle className="voorbeeld-article-space ">
+          <UtrechtSpotlightSection className="utrecht-spotlight-section-wmebv">
+            <UtrechtHeading1>
+              <CircleCheck /> Vraag met succes verstuurd
+            </UtrechtHeading1>
+            <UtrechtParagraph>Kenmerk: 230829-1118-59dc</UtrechtParagraph>
+          </UtrechtSpotlightSection>
+          <UtrechtHeading1>Wat gaat er nu gebeuren?</UtrechtHeading1>
+
+          <UnorderedList>
+            <UnorderedListItem className="voorbeeld-unordered-list-item">
+              U ontvangt een bevestigingsmail op <span className="bold-mail">j.vandrouwen@gmail.com</span>
+            </UnorderedListItem>
+            <UnorderedListItem className="voorbeeld-unordered-list-item">
+              De afdeling Vraagbaak gaat met uw vraag aan de slag.
+            </UnorderedListItem>
+          </UnorderedList>
+          <UtrechtLink className="link">
+            <Printer /> Print uw vraag
+          </UtrechtLink>
+          <UtrechtLink className="link">
+            <FileText /> Download uw vraag als PDF
+          </UtrechtLink>
+          <UtrechtLink className="link">Terug naar voorbeeld.nl</UtrechtLink>
+
+          <UtrechtButtonLink href="../Home" appearance="primary-action-button">
+            Doorgaan
+          </UtrechtButtonLink>
+        </UtrechtArticle>
+      </UtrechtPageContent>
+      <ExampleFooter></ExampleFooter>
+    </UtrechtPage>
+  );
+}
