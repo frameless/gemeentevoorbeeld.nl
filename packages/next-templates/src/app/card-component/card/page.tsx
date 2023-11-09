f3a64bd1a94088cb8f7c035c2427d961c4f248f4
scar055 <rowanvanlaar@gmail.com>
2023-11-09 15:56:23 +0100

    🎨 card in een if statement gestopt

--- packages/next-templates/src/app/card-component/card/page.tsx
+++ packages/next-templates/src/app/card-component/card/page.tsx
@@ -34,47 +34,61 @@ export const CardListItem = ({
   ...props
 }: PropsWithChildren<CardListItemProps>) => {
   const linkRef = React.useRef<HTMLAnchorElement>(null);
-
-  return (
-    <li {...props} className={clsx('utrecht-card-list-item', props.className)} onClick={() => linkRef.current?.click()}>
-      {imageSrc && imageAlt && (
-        <Image
-          src={imageSrc}
-          alt={imageAlt}
-          height={imageHeight}
-          width={imageWidth}
-          className={'utrecht-card-list-item__image'}
-        />
-      )}
-      {cardRole ? (
-        <article>
-          <div className={'utrecht-card-list-item__content'}>
-            <hgroup>
-              <Heading level={headingLevel} className="utrecht-card-list-item__title">
-                <Link ref={linkRef} href={href} className="utrecht-link utrecht-link--html-a">
-                  {title}
-                </Link>
-              </Heading>
-              {preHeading && <p className="utrecht-card-list-item__pre-heading">{preHeading}</p>}
-            </hgroup>
-            {children}
-          </div>
-        </article>
-      ) : (
-        <div className={'utrecht-card-list-item__content'}>
-          <hgroup>
-            <Heading level={headingLevel} className="utrecht-card-list-item__title">
-              <Link ref={linkRef} href={href} className="utrecht-link utrecht-link--html-a">
-                {title}
-              </Link>
-            </Heading>
-            {preHeading && <p className="utrecht-card-list-item__pre-heading">{preHeading}</p>}
-          </hgroup>
-          {children}
-        </div>
-      )}
-    </li>
+  let card = (
+    <div className={'utrecht-card-list-item__content'}>
+      <hgroup>
+        <Heading level={headingLevel} className="utrecht-card-list-item__title">
+          <Link ref={linkRef} href={href} className="utrecht-link utrecht-link--html-a">
+            {title}
+          </Link>
+        </Heading>
+        {preHeading && <p className="utrecht-card-list-item__pre-heading">{preHeading}</p>}
+      </hgroup>
+      {children}
+    </div>
   );
+
+  if (cardRole) {
+    card = (
+      <li
+        {...props}
+        className={clsx('utrecht-card-list-item', props.className)}
+        onClick={() => linkRef.current?.click()}
+      >
+        {imageSrc && imageAlt && (
+          <Image
+            src={imageSrc}
+            alt={imageAlt}
+            height={imageHeight}
+            width={imageWidth}
+            className={'utrecht-card-list-item__image'}
+          />
+        )}
+        <article>{card}</article>
+      </li>
+    );
+  } else {
+    card = (
+      <li
+        {...props}
+        className={clsx('utrecht-card-list-item', props.className)}
+        onClick={() => linkRef.current?.click()}
+      >
+        {imageSrc && imageAlt && (
+          <Image
+            src={imageSrc}
+            alt={imageAlt}
+            height={imageHeight}
+            width={imageWidth}
+            className={'utrecht-card-list-item__image'}
+          />
+        )}
+        {card}
+      </li>
+    );
+  }
+
+  return card;
 };
 export default function Home() {
   return (
