04bf014257c63dc51388b8b135f1e08a2e168d27
MeesoPost <100311542+MeesoPost@users.noreply.github.com>
2023-11-15 15:04:12 +0100

    🎨 added footer block divs

--- packages/next-templates/src/components/wmebv/Footer/ExampleFooterWmebv.tsx
+++ packages/next-templates/src/components/wmebv/Footer/ExampleFooterWmebv.tsx
@@ -1,26 +1,34 @@
 import Logo from '../../../app/styling/assets/voorbeeld-footer.svg';
 import { NavigationList } from '../../NavigationList/Navigationlist';
 import { NavigationListItem } from '../../NavigationList/NavigationListItem';
-import { Heading5, PageFooter, Paragraph } from '@utrecht/component-library-react';
+import { Heading2, Heading5, PageFooter, Paragraph, UnorderedListItem } from '@utrecht/component-library-react';
 import React, { HTMLAttributes } from 'react';
 import '../../ExampleFooter/footer.css';
+import { UtrechtHeading2, UtrechtLink, UtrechtParagraph } from '@utrecht/web-component-library-react';
 
 interface ExampleFooterWmebvProps extends HTMLAttributes<HTMLDivElement> {}
 export const ExampleFooterWmebv = ({ ...props }: ExampleFooterWmebvProps) => (
   <PageFooter>
     <div className="utrecht-page-footer__content">
       <div className="example-link-list-container" {...props}>
-        <Logo className="example--footer-logo" />
-        <NavigationList>
-          <NavigationListItem className="utrecht-link-list__item" href="#" label="Contact" />
-          <NavigationListItem className="utrecht-link-list__item" href="#" label="RSS" />
-        </NavigationList>
-        <NavigationList>
-          <NavigationListItem className="utrecht-link-list__item" href="#" label="Bescherming persoonsgegevens" />
-          <NavigationListItem className="utrecht-link-list__item" href="#" label="Gebruikersvoorwaarden" />
-          <NavigationListItem className="utrecht-link-list__item" href="#" label="Proclaimer" />
-          <NavigationListItem className="utrecht-link-list__item" href="#" label="Cookieverklaring" />
-        </NavigationList>
+        <div className="voorbeeld-footer-block">
+          <Logo className="example--footer-logo" />
+        </div>
+        <div className="voorbeeld-footer-block utrecht-link-list__item">
+          <UtrechtHeading2 className="voorbeeld-footer-heading">Contact</UtrechtHeading2>
+          <UtrechtParagraph>
+            Bel <UtrechtLink href="tel:453453">453 453</UtrechtLink> (maandag tot en met vrijdag van 09:00 tot 17:00) of
+            stuur een email naar <UtrechtLink href="mailto:vragen@voorbeeld.nl">vragen@voorbeeld.nl</UtrechtLink>
+          </UtrechtParagraph>
+        </div>
+        <div className="voorbeeld-footer-block">
+          <NavigationList>
+            <NavigationListItem className="utrecht-link-list__item" href="#" label="Bescherming persoonsgegevens" />
+            <NavigationListItem className="utrecht-link-list__item" href="#" label="Gebruikersvoorwaarden" />
+            <NavigationListItem className="utrecht-link-list__item" href="#" label="Proclaimer" />
+            <NavigationListItem className="utrecht-link-list__item" href="#" label="Cookieverklaring" />
+          </NavigationList>
+        </div>
       </div>
     </div>
   </PageFooter>
