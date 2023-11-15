2a6ef0a2c0f9c29b6f728476e98a569d863f368a
Robbert Broersma <git@robbertbroersma.nl>
2023-11-15 21:13:38 +0100

    ✨ fix link to /mijn-omgeving/

--- packages/next-templates/src/app/wmebv/ingelogd/stap3/page.tsx
+++ packages/next-templates/src/app/wmebv/ingelogd/stap3/page.tsx
@@ -29,7 +29,7 @@ import { ExampleHeaderFunnelWmebv } from '@/components/wmebv/Header/ExampleHeade
 export default function home() {
   const userdata = {
     username: 'J. van Drouwen',
-    userURL: '#',
+    userURL: '/mijn-omgeving/',
   };
   const [storedData, _, __, removeStoredData] = useSessionState<ContactFormSessionData>(FORM_SESSION_KEY, {});
   return (
