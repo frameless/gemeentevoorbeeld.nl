34dd4e9d7b0d7bbb5c1054c73bee206c64f7b3d9
Robbert Broersma <git@robbertbroersma.nl>
2023-11-16 04:36:54 +0100

    ♿️ mark message textareas as required

--- packages/next-templates/src/app/wmebv/ingelogd/stap1/page.tsx
+++ packages/next-templates/src/app/wmebv/ingelogd/stap1/page.tsx
@@ -95,6 +95,7 @@ export default function home() {
               label="Stel uw vraag"
               invalid={!!errors[messageField.name]}
               errorMessage={String(errors[messageField.name]?.message)}
+              required
             ></FormFieldTextarea>
 
             <FormFieldFile
