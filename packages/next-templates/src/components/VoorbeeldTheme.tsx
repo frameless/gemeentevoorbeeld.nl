16e98bc582dd07f55f431b2a182e02e40def74a9
MeesoPost <100311542+MeesoPost@users.noreply.github.com>
2023-11-21 15:05:06 +0100

    🐛 fix build error bug

--- packages/next-templates/src/components/VoorbeeldTheme.tsx
+++ packages/next-templates/src/components/VoorbeeldTheme.tsx
@@ -1,6 +1,6 @@
 import { PropsWithChildren } from 'react';
 import '@nl-design-system-unstable/voorbeeld-design-tokens/dist/index.css';
-import '@gemeente-denhaag/design-tokens-components';
+import '@gemeente-denhaag/design-tokens-components/dist/theme/index.css';
 import '@/app/font';
 import './VoorbeeldTheme.scss';
 import '../app/globals.css';
