208720295b09582f43b62133428f2c9d1f55bdbd
ryanvvalkenhoef <76593083+ryanvvalkenhoef@users.noreply.github.com>
2024-01-08 18:03:14 +0100

    Created homepage and react component TopTask

--- packages/components-react/src/index.ts
+++ packages/components-react/src/index.ts
@@ -1,1 +1,3 @@
 export { Button } from './Button';
+export { TopTaskNavigation } from '../../next-templates/src/components/TopTaskNavigation';
+export { TopTaskLink } from '../../next-templates/src/components/TopTaskLink';
