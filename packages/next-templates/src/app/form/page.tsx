5209472d9dc88458b402c42a00883303a2827870
ryanvvalkenhoef <76593083+ryanvvalkenhoef@users.noreply.github.com>
2024-01-16 11:20:59 +0100

    :truck: Moved graffiti-melding form to meldingen-openbare-ruimte

--- packages/next-templates/src/app/form/page.tsx
+++ packages/next-templates/src/app/form/page.tsx
@@ -24,7 +24,7 @@ import {
   UnorderedListItem,
 } from '@utrecht/component-library-react';
 import '@/app/styling/css/form-page.css';
-import BacklinkIcon from '@/app/styling/assets/backlink-icon.svg';
+import { IconChevronLeft } from '@tabler/icons-react';
 import { useForm } from 'react-hook-form';
 import { ExampleHeaderFunnel } from '@/components/ExampleHeader/ExampleHeaderFunnel/ExampleHeaderFunnel';
 import { ExampleFooterFocus } from '@/components/ExampleFooter/ExampleFooterFocus/ExampleFooterFocus';
@@ -43,8 +43,8 @@ export default function Home() {
         <ExampleHeaderFunnel />
         <PageContent className="voorbeeld-page-content-flex">
           <Article className="voorbeeld-article-space">
-            <Link className="example--back-link" href="./">
-              <BacklinkIcon />
+            <Link className="example--back-link" href="./graffiti-melding">
+              <IconChevronLeft />
               &nbsp;&nbsp;<span className="example--underlined">Terug</span>
             </Link>
             <Heading1 className="utrecht-heading-1">Melding openbare ruimte</Heading1>
