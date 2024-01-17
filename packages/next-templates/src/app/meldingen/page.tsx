ea5ce49a817d07f1964e1962d74d9b291cb460cc
ryanvvalkenhoef <76593083+ryanvvalkenhoef@users.noreply.github.com>
2024-01-17 16:55:50 +0100

    :fire: Removed unused imports

--- packages/next-templates/src/app/meldingen/page.tsx
+++ packages/next-templates/src/app/meldingen/page.tsx
@@ -2,17 +2,6 @@
 import { Article, Page, PageContent } from '@utrecht/component-library-react';
 import { TopTaskNavigation } from '../components/TopTask/TopTaskNavigation';
 import { TopTaskLink } from '../components/TopTask/TopTaskLink';
-import {
-  UtrechtIconAfvalScheiden,
-  UtrechtIconMeldingKlacht,
-  UtrechtIconNummerbord,
-  UtrechtIconPaspoort,
-  UtrechtIconVerhuizen,
-  UtrechtIconWerken,
-} from '@utrecht/web-component-library-react';
-import '@/app/styling/css/detail-page.css';
-import '@utrecht/component-library-css';
-import '@utrecht/design-tokens/dist/index.css';
 import { ExampleHeader } from '@/components/ExampleHeader/ExampleHeader';
 import { ExampleFooter } from '@/components/ExampleFooter/ExampleFooter';
 import { ExampleNavigation } from '@/components/ExampleNavigation/ExampleNavigation';
