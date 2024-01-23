4cd471da9d6315c174b355c13da31f4fa6fa60a3
ryanvvalkenhoef <76593083+ryanvvalkenhoef@users.noreply.github.com>
2024-01-23 13:58:38 +0100

    :art: Added sections around Heading2 and FormFields

--- packages/next-templates/src/app/meldingen/graffiti-melding/form/page.tsx
+++ packages/next-templates/src/app/meldingen/graffiti-melding/form/page.tsx
@@ -71,169 +71,175 @@ export default function Home() {
               Vul alle velden in. Als een veld optioneel is, staat dit erbij
             </Paragraph>
             <form method="POST" noValidate onSubmit={handleSubmit((data) => {})}>
-              <Heading2 utrecht-heading-2>Wat wilt u melden?</Heading2>
-              <Paragraph className="voorbeeld-title-paragraph">Kies een categorie die bij uw melding past.</Paragraph>
-              <FormField invalid={!!errors.mainCategory}>
-                <FormLabel className="voorbeeld-form-label" htmlFor="mainCategory">
-                  Hoofdcategorie
-                </FormLabel>
-                <Select
-                  aria-label="Hoofdcategorie"
-                  className="voorbeeld-select"
-                  defaultValue=""
-                  id="mainCategory"
-                  {...register('mainCategory', {
-                    required: 'Dit is verplicht',
-                  })}
-                  placeholder="Kies een hoofdcategorie"
-                  invalid={!!errors.mainCategory}
-                >
-                  <SelectOption disabled value="">
-                    Kies een hoofdcategorie
-                  </SelectOption>
-                  <SelectOption value="1">Option #1</SelectOption>
-                  <SelectOption value="2">Option #2</SelectOption>
-                  <SelectOption value="3">Option #3</SelectOption>
-                  <SelectOption value="4">Option #4</SelectOption>
-                </Select>
-                <FormFieldDescription invalid={!!errors.mainCategory}>
-                  {errors.mainCategory?.message}
-                </FormFieldDescription>
-              </FormField>
-              <FormField className="voorbeeld-formField" invalid={!!errors.subCategory}>
-                <FormLabel className="voorbeeld-form-label" htmlFor="subCategory">
-                  Subcategorie
-                </FormLabel>
-                <Select
-                  aria-label="Subcategorie"
-                  defaultValue=""
-                  className="voorbeeld-select"
-                  id="subCategory"
-                  {...register('subCategory', {
-                    required: 'Dit is verplicht',
-                  })}
-                  placeholder="Kies een subcategorie"
-                  invalid={!!errors.subCategory}
-                >
-                  <SelectOption disabled value="">
-                    Kies een subcategorie
-                  </SelectOption>
-                  <SelectOption value="1">Option #1</SelectOption>
-                  <SelectOption value="2">Option #2</SelectOption>
-                  <SelectOption value="3">Option #3</SelectOption>
-                  <SelectOption value="4">Option #4</SelectOption>
-                </Select>
-                <FormFieldDescription invalid={!!errors.subCategory}>
-                  {errors.subCategory?.message}
-                </FormFieldDescription>
-              </FormField>
-              <FormFieldTextarea
-                description="Beschrijf hier wat er aan de hand is en eventueel wat er aan gedaan kan worden."
-                className="voorbeeld-textarea"
-                id={`field-${descriptionField.name}`}
-                aria-label="Omschrijving"
-                label="Omschrijving"
-                invalid={!!errors[descriptionField.name]}
-                errorMessage={String(errors[descriptionField.name]?.message)}
-                {...descriptionField}
-              />
-              <div className="voorbeeld-bijlage-flex-container">
-                <Paragraph className="voorbeeld-paragraph-bijlage">Bijlage</Paragraph>
-                <Paragraph>(optioneel)</Paragraph>
-              </div>
-              <UnorderedList className="voorbeeld-unordered-list-space">
-                <UnorderedListItem>Bestanden moeten kleiner zijn dan 10 MB.</UnorderedListItem>
-                <UnorderedListItem>Toegestane bestandstypen: gif, jpg, jpeg, png.</UnorderedListItem>
-              </UnorderedList>
-              <div className="voorbeeld-bijlage-flex-container">
-                <Button appearance="secondary-action-button">Bestand kiezen</Button>
-                <Paragraph className="voorbeeld-paragraph-bijlage">Geen bestand gekozen</Paragraph>
-              </div>
-              <Heading2>Op welke locatie heeft de melding betrekking?</Heading2>
-              <FormFieldTextbox
-                id={`place-${placeField.name}`}
-                className="voorbeeld-textbox"
-                invalid={!!errors[placeField.name]}
-                errorMessage={String(errors[placeField.name]?.message)}
-                aria-label="Plaats"
-                label="Plaats"
-                {...placeField}
-              />
-              <FormFieldTextbox
-                id={`street-${streetField.name}`}
-                className="voorbeeld-textbox"
-                invalid={!!errors[streetField.name]}
-                errorMessage={String(errors[streetField.name]?.message)}
-                aria-label="Straat"
-                label="Straat"
-                {...streetField}
-              />
-              <FormFieldTextbox
-                id={`height-${heightField.name}`}
-                className="voorbeeld-textbox"
-                invalid={!!errors[heightField.name]}
-                description="Bijvoorbeeld het huisnummer, parkeerplaats of speeltuintje."
-                errorMessage={String(errors[heightField.name]?.message)}
-                aria-label="Ter hoogte van"
-                label="Ter hoogte van"
-                {...heightField}
-              />
-              <Heading2>Hoe kunnen we u bereiken voor meer informatie?</Heading2>
-              <Fieldset role="radiogroup">
-                <FieldsetLegend>Anoniem Melden?</FieldsetLegend>
-                <FormFieldDescription className="voorbeeld-FormFieldDescription--spacing">
-                  Als u iets anoniem meld kunnen we u niet informeren over het verloop van de afhandeling voor deze
-                  melding.
-                </FormFieldDescription>
-                <FormField className="voorbeeld-formField" type="radio">
-                  <FormLabel className="voorbeeld-radio-button-form-label" htmlFor="anoniem-ja" type="radio">
-                    Ja
+              <section className="voorbeeld-section--spacing">
+                <Heading2 utrecht-heading-2>Wat wilt u melden?</Heading2>
+                <Paragraph className="voorbeeld-title-paragraph">Kies een categorie die bij uw melding past.</Paragraph>
+                <FormField invalid={!!errors.mainCategory}>
+                  <FormLabel className="voorbeeld-form-label" htmlFor="mainCategory">
+                    Hoofdcategorie
                   </FormLabel>
-                  <RadioButton className="utrecht-form-field__input" id="anoniem-ja" name="anoniem" value="1" />
+                  <Select
+                    aria-label="Hoofdcategorie"
+                    className="voorbeeld-select"
+                    defaultValue=""
+                    id="mainCategory"
+                    {...register('mainCategory', {
+                      required: 'Dit is verplicht',
+                    })}
+                    placeholder="Kies een hoofdcategorie"
+                    invalid={!!errors.mainCategory}
+                  >
+                    <SelectOption disabled value="">
+                      Kies een hoofdcategorie
+                    </SelectOption>
+                    <SelectOption value="1">Option #1</SelectOption>
+                    <SelectOption value="2">Option #2</SelectOption>
+                    <SelectOption value="3">Option #3</SelectOption>
+                    <SelectOption value="4">Option #4</SelectOption>
+                  </Select>
+                  <FormFieldDescription invalid={!!errors.mainCategory}>
+                    {errors.mainCategory?.message}
+                  </FormFieldDescription>
                 </FormField>
-                <FormField className="voorbeeld-formField" type="radio">
-                  <FormLabel className="voorbeeld-radio-button-form-label" htmlFor="anoniem-nee" type="radio">
-                    Nee
+                <FormField className="voorbeeld-formField" invalid={!!errors.subCategory}>
+                  <FormLabel className="voorbeeld-form-label" htmlFor="subCategory">
+                    Subcategorie
                   </FormLabel>
-                  <RadioButton className="utrecht-form-field__input" id="anoniem-nee" name="anoniem" value="2" />
+                  <Select
+                    aria-label="Subcategorie"
+                    defaultValue=""
+                    className="voorbeeld-select"
+                    id="subCategory"
+                    {...register('subCategory', {
+                      required: 'Dit is verplicht',
+                    })}
+                    placeholder="Kies een subcategorie"
+                    invalid={!!errors.subCategory}
+                  >
+                    <SelectOption disabled value="">
+                      Kies een subcategorie
+                    </SelectOption>
+                    <SelectOption value="1">Option #1</SelectOption>
+                    <SelectOption value="2">Option #2</SelectOption>
+                    <SelectOption value="3">Option #3</SelectOption>
+                    <SelectOption value="4">Option #4</SelectOption>
+                  </Select>
+                  <FormFieldDescription invalid={!!errors.subCategory}>
+                    {errors.subCategory?.message}
+                  </FormFieldDescription>
                 </FormField>
-              </Fieldset>
-              <FormFieldTextbox
-                id={`name-${nameField.name}`}
-                className="voorbeeld-textbox"
-                invalid={!!errors[nameField.name]}
-                required
-                type="name"
-                autoComplete="name"
-                errorMessage={String(errors[nameField.name]?.message)}
-                aria-label="Naam"
-                label="Naam"
-                {...nameField}
-              />
-              <FormFieldTextbox
-                id={`email-${emailField.name}`}
-                className="voorbeeld-textbox"
-                invalid={!!errors[emailField.name]}
-                autoComplete="email"
-                type="email"
-                required
-                errorMessage={String(errors[emailField.name]?.message)}
-                aria-label="E-mailadres"
-                label="E-mailadres"
-                {...emailField}
-              />
-              <FormFieldTextbox
-                id={`phone-${phoneField.name}`}
-                className="voorbeeld-textbox"
-                invalid={!!errors[phoneField.name]}
-                type="tel"
-                autoComplete="tel"
-                description="Niet verplicht."
-                errorMessage={String(errors[phoneField.name]?.message)}
-                aria-label="Telefoonnummer"
-                label="Telefoonnummer"
-                {...phoneField}
-              />
+                <FormFieldTextarea
+                  description="Beschrijf hier wat er aan de hand is en eventueel wat er aan gedaan kan worden."
+                  className="voorbeeld-textarea"
+                  id={`field-${descriptionField.name}`}
+                  aria-label="Omschrijving"
+                  label="Omschrijving"
+                  invalid={!!errors[descriptionField.name]}
+                  errorMessage={String(errors[descriptionField.name]?.message)}
+                  {...descriptionField}
+                />
+                <div className="voorbeeld-bijlage-flex-container">
+                  <Paragraph className="voorbeeld-paragraph-bijlage">Bijlage</Paragraph>
+                  <Paragraph>(optioneel)</Paragraph>
+                </div>
+                <UnorderedList className="voorbeeld-unordered-list-space">
+                  <UnorderedListItem>Bestanden moeten kleiner zijn dan 10 MB.</UnorderedListItem>
+                  <UnorderedListItem>Toegestane bestandstypen: gif, jpg, jpeg, png.</UnorderedListItem>
+                </UnorderedList>
+                <div className="voorbeeld-bijlage-flex-container">
+                  <Button appearance="secondary-action-button">Bestand kiezen</Button>
+                  <Paragraph className="voorbeeld-paragraph-bijlage">Geen bestand gekozen</Paragraph>
+                </div>
+              </section>
+              <section className="voorbeeld-section--spacing">
+                <Heading2>Op welke locatie heeft de melding betrekking?</Heading2>
+                <FormFieldTextbox
+                  id={`place-${placeField.name}`}
+                  className="voorbeeld-textbox"
+                  invalid={!!errors[placeField.name]}
+                  errorMessage={String(errors[placeField.name]?.message)}
+                  aria-label="Plaats"
+                  label="Plaats"
+                  {...placeField}
+                />
+                <FormFieldTextbox
+                  id={`street-${streetField.name}`}
+                  className="voorbeeld-textbox"
+                  invalid={!!errors[streetField.name]}
+                  errorMessage={String(errors[streetField.name]?.message)}
+                  aria-label="Straat"
+                  label="Straat"
+                  {...streetField}
+                />
+                <FormFieldTextbox
+                  id={`height-${heightField.name}`}
+                  className="voorbeeld-textbox"
+                  invalid={!!errors[heightField.name]}
+                  description="Bijvoorbeeld het huisnummer, parkeerplaats of speeltuintje."
+                  errorMessage={String(errors[heightField.name]?.message)}
+                  aria-label="Ter hoogte van"
+                  label="Ter hoogte van"
+                  {...heightField}
+                />
+              </section>
+              <section className="voorbeeld-section--spacing">
+                <Heading2>Hoe kunnen we u bereiken voor meer informatie?</Heading2>
+                <Fieldset role="radiogroup">
+                  <FieldsetLegend>Anoniem Melden?</FieldsetLegend>
+                  <FormFieldDescription className="voorbeeld-FormFieldDescription--spacing">
+                    Als u iets anoniem meld kunnen we u niet informeren over het verloop van de afhandeling voor deze
+                    melding.
+                  </FormFieldDescription>
+                  <FormField className="voorbeeld-formField" type="radio">
+                    <FormLabel className="voorbeeld-radio-button-form-label" htmlFor="anoniem-ja" type="radio">
+                      Ja
+                    </FormLabel>
+                    <RadioButton className="utrecht-form-field__input" id="anoniem-ja" name="anoniem" value="1" />
+                  </FormField>
+                  <FormField className="voorbeeld-formField" type="radio">
+                    <FormLabel className="voorbeeld-radio-button-form-label" htmlFor="anoniem-nee" type="radio">
+                      Nee
+                    </FormLabel>
+                    <RadioButton className="utrecht-form-field__input" id="anoniem-nee" name="anoniem" value="2" />
+                  </FormField>
+                </Fieldset>
+                <FormFieldTextbox
+                  id={`name-${nameField.name}`}
+                  className="voorbeeld-textbox"
+                  invalid={!!errors[nameField.name]}
+                  required
+                  type="name"
+                  autoComplete="name"
+                  errorMessage={String(errors[nameField.name]?.message)}
+                  aria-label="Naam"
+                  label="Naam"
+                  {...nameField}
+                />
+                <FormFieldTextbox
+                  id={`email-${emailField.name}`}
+                  className="voorbeeld-textbox"
+                  invalid={!!errors[emailField.name]}
+                  autoComplete="email"
+                  type="email"
+                  required
+                  errorMessage={String(errors[emailField.name]?.message)}
+                  aria-label="E-mailadres"
+                  label="E-mailadres"
+                  {...emailField}
+                />
+                <FormFieldTextbox
+                  id={`phone-${phoneField.name}`}
+                  className="voorbeeld-textbox"
+                  invalid={!!errors[phoneField.name]}
+                  type="tel"
+                  autoComplete="tel"
+                  description="Niet verplicht."
+                  errorMessage={String(errors[phoneField.name]?.message)}
+                  aria-label="Telefoonnummer"
+                  label="Telefoonnummer"
+                  {...phoneField}
+                />
+              </section>
               <Button className="voorbeeld-button--spacing" type="submit" appearance="primary-action-button">
                 Versturen
               </Button>
