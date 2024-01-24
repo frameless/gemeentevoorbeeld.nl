7dcd40796e30ae0378af978c4ddc5defc19f7d2c
ryanvvalkenhoef <76593083+ryanvvalkenhoef@users.noreply.github.com>
2024-01-24 11:55:52 +0100

    :recycle: Changed a-element to Utrecht Link-component

--- packages/next-templates/src/components/TopTask/TopTaskLink.tsx
+++ packages/next-templates/src/components/TopTask/TopTaskLink.tsx
@@ -4,9 +4,9 @@
  * Copyright (c) 2024 Ryan van Valkenhoef
  */
 
-import clsx from 'clsx';
 import { AnchorHTMLAttributes, ForwardedRef, forwardRef, PropsWithChildren, ReactNode } from 'react';
 import './TopTaskLink.css';
+import { Link } from '@utrecht/component-library-react';
 
 // Somehow `placeholder` incorrectly is a global HTML attribute in React, ignore that
 export interface LinkProps extends Omit<AnchorHTMLAttributes<HTMLAnchorElement>, 'placeholder'> {
@@ -35,27 +35,28 @@ export const TopTaskLink = forwardRef(
     // since it is primarily a basis for implementing input[href^="tel"].
     // Telephone number rendering in React is best achieved using composition
     // of the TelephoneValue component.
-    <a
-      href={placeholder ? '#' : href}
-      ref={ref}
-      role={role || (placeholder ? 'link' : undefined)}
-      className={clsx(
-        'example-toptask-link',
-        'example-toptask-link--html-a',
-        {
-          'example-toptask-link--box-content': boxContent,
-          'example-toptask-link--external': external,
-          'example-toptask-link--placeholder': placeholder,
-        },
-        className,
-      )}
-      aria-disabled={placeholder ? 'true' : undefined}
-      rel={external ? 'external noopener noreferrer' : undefined}
-      {...restProps}
-    >
+    <Link className="example-toptask-link" href={placeholder ? '#' : href}>
+      {/* // <a
+    //   href={placeholder ? '#' : href}
+    //   ref={ref}
+    //   role={role || (placeholder ? 'link' : undefined)}
+    //   className={clsx(
+    //     'example-toptask-link',
+    //     'example-toptask-link--html-a',
+    //     {
+    //       'example-toptask-link--box-content': boxContent,
+    //       'example-toptask-link--external': external,
+    //       'example-toptask-link--placeholder': placeholder,
+    //     },
+    //     className,
+    //   )}
+    //   aria-disabled={placeholder ? 'true' : undefined}
+    //   rel={external ? 'external noopener noreferrer' : undefined}
+    //   {...restProps}
+    // > */}
       {icon && <span className="example-toptask-link__icon">{icon}</span>}
       <span className="example-toptask-link__title">{children}</span>
-    </a>
+    </Link>
   ),
 );
 
