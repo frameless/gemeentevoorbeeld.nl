16aaa2476ee7abc58f51a4ddfb0ba67977c3d102
ryanvvalkenhoef <76593083+ryanvvalkenhoef@users.noreply.github.com>
2024-01-26 15:19:05 +0100

    :pencil2: Moved className to another component

--- packages/next-templates/src/app/meldingen/eenhoornparkeerzone-melding/form/page.tsx
+++ packages/next-templates/src/app/meldingen/eenhoornparkeerzone-melding/form/page.tsx
@@ -50,116 +50,118 @@ export default function Home() {
                 router.push('./confirmed');
               })}
             >
-              <FormField invalid={!!errors.location}>
-                <FormLabel className="voorbeeld-formField" htmlFor="location">
-                  Locatie:
-                </FormLabel>
-                <Textbox
-                  id="location"
-                  {...register('location', {
-                    required: 'Dit is verplicht',
-                    minLength: { value: 4, message: 'Min length is 4' },
-                  })}
-                  placeholder="Vul hier een locatie in"
-                  invalid={!!errors.location}
-                />
-                <Paragraph>{errors.location?.message}</Paragraph>
-              </FormField>
-              <FormField className="voorbeeld-formField" invalid={!!errors.description}>
-                <FormLabel htmlFor="description">Beschrijving:</FormLabel>
-                <Textarea
-                  id="description"
-                  invalid={!!errors.description}
-                  {...register('description', {
-                    required: 'Dit is verplicht',
-                    minLength: { value: 4, message: 'Min length is 4' },
-                  })}
-                  placeholder="Vul hier een beschrijving in"
-                />
-                <Paragraph>{errors.description?.message}</Paragraph>
-              </FormField>
-              <FormField className="voorbeeld-formField" invalid={!!errors.enclosedAerialView}>
-                <FormLabel htmlFor="enclosed-aerial-view">Bijgevoegde luchtfoto&apos;s:</FormLabel>
-                <Textbox
-                  id="enclosed-aerial-view"
-                  invalid={!!errors.enclosedAerialView}
-                  {...register('enclosedAerialView', {
-                    required: 'Dit is verplicht',
-                    minLength: { value: 4, message: 'Min length is 4' },
-                  })}
-                  placeholder="Upload hier uw luchtfoto's"
-                />
-                <Paragraph>{errors.enclosedAerialView?.message}</Paragraph>
-              </FormField>
-              <Fieldset id="df861ef1-844a-42df-8365-b54f59474fb8" role="radiogroup">
-                <FieldsetLegend>
-                  Hoe snel denkt u dat de eenhoorns en vliegende huisdieren hulp nodig hebben?
-                </FieldsetLegend>
-                <FormField type="radio" className="radio-field">
-                  <FormLabel htmlFor="right-now" type="radio">
-                    <RadioButton
-                      className="utrecht-form-field__input"
-                      id="right-now"
-                      name="828aa90e-0e99-4db9-a5e5-d39f689cf7b3"
-                      value="1"
-                    />
-                    Nu meteen
-                  </FormLabel>
+              <section className="voorbeeld-section--spacing">
+                <FormField className="voorbeeld-formField" invalid={!!errors.location}>
+                  <FormLabel htmlFor="location">Locatie:</FormLabel>
+                  <Textbox
+                    id="location"
+                    {...register('location', {
+                      required: 'Dit is verplicht',
+                      minLength: { value: 4, message: 'Min length is 4' },
+                    })}
+                    placeholder="Vul hier een locatie in"
+                    invalid={!!errors.location}
+                  />
+                  <Paragraph>{errors.location?.message}</Paragraph>
                 </FormField>
-                <FormField type="radio" className="radio-field">
-                  <FormLabel htmlFor="shortly" type="radio">
-                    <RadioButton
-                      className="utrecht-form-field__input"
-                      id="shortly"
-                      name="828aa90e-0e99-4db9-a5e5-d39f689cf7b3"
-                      value="2"
-                    />
-                    Binnenkort
-                  </FormLabel>
+                <FormField className="voorbeeld-formField" invalid={!!errors.description}>
+                  <FormLabel htmlFor="description">Beschrijving:</FormLabel>
+                  <Textarea
+                    id="description"
+                    invalid={!!errors.description}
+                    {...register('description', {
+                      required: 'Dit is verplicht',
+                      minLength: { value: 4, message: 'Min length is 4' },
+                    })}
+                    placeholder="Vul hier een beschrijving in"
+                  />
+                  <Paragraph>{errors.description?.message}</Paragraph>
                 </FormField>
-                <FormField type="radio" className="radio-field">
-                  <FormLabel htmlFor="when-pigs-can-fly" type="radio">
-                    <RadioButton
-                      className="utrecht-form-field__input"
-                      id="when-pigs-can-fly"
-                      name="828aa90e-0e99-4db9-a5e5-d39f689cf7b3"
-                      value="3"
-                    />
-                    Wanneer varkens kunnen vliegen
-                  </FormLabel>
+                <FormField className="voorbeeld-formField" invalid={!!errors.enclosedAerialView}>
+                  <FormLabel htmlFor="enclosed-aerial-view">Bijgevoegde luchtfoto&apos;s:</FormLabel>
+                  <Textbox
+                    id="enclosed-aerial-view"
+                    invalid={!!errors.enclosedAerialView}
+                    {...register('enclosedAerialView', {
+                      required: 'Dit is verplicht',
+                      minLength: { value: 4, message: 'Min length is 4' },
+                    })}
+                    placeholder="Upload hier uw luchtfoto's"
+                  />
+                  <Paragraph>{errors.enclosedAerialView?.message}</Paragraph>
                 </FormField>
-              </Fieldset>
-              <FormLabel>Contactgegevens:</FormLabel>
-              <Paragraph>
-                Laat uw naam en e-mailadres achter, zodat we u op de hoogte kunnen houden van eventuele magische
-                ontwikkelingen.
-              </Paragraph>
-              <FormField className="voorbeeld-formField" invalid={!!errors.fullName}>
-                <FormLabel htmlFor="fullName">Volledige naam:</FormLabel>
-                <Textbox
-                  invalid={!!errors.fullName}
-                  {...register('fullName', {
-                    required: 'Dit is verplicht',
-                    minLength: { value: 4, message: 'Min length is 4' },
-                  })}
-                  placeholder="Vul hier uw volledige naam in"
-                  id="fullName"
-                />
-              </FormField>
-              <Paragraph>{errors.fullName?.message}</Paragraph>
-              <FormField className="voorbeeld-formField" invalid={!!errors.emailAdress}>
-                <FormLabel htmlFor="emailAdress">E-mailadres:</FormLabel>
-                <Textbox
-                  invalid={!!errors.emailAdress}
-                  {...register('emailAdress', { required: 'Dit is verplicht' })}
-                  placeholder="Vul hier uw e-mailadres in"
-                  id="emailAdress"
-                />
-              </FormField>
-              <Paragraph>{errors.emailAdress?.message}</Paragraph>
-              <Button className="voorbeeld-button--spacing" type="submit" appearance="primary-action-button">
-                Dien uw melding in
-              </Button>
+                <Fieldset id="df861ef1-844a-42df-8365-b54f59474fb8" role="radiogroup">
+                  <FieldsetLegend>
+                    Hoe snel denkt u dat de eenhoorns en vliegende huisdieren hulp nodig hebben?
+                  </FieldsetLegend>
+                  <FormField type="radio" className="radio-field">
+                    <FormLabel htmlFor="right-now" type="radio">
+                      <RadioButton
+                        className="utrecht-form-field__input"
+                        id="right-now"
+                        name="828aa90e-0e99-4db9-a5e5-d39f689cf7b3"
+                        value="1"
+                      />
+                      Nu meteen
+                    </FormLabel>
+                  </FormField>
+                  <FormField type="radio" className="radio-field">
+                    <FormLabel htmlFor="shortly" type="radio">
+                      <RadioButton
+                        className="utrecht-form-field__input"
+                        id="shortly"
+                        name="828aa90e-0e99-4db9-a5e5-d39f689cf7b3"
+                        value="2"
+                      />
+                      Binnenkort
+                    </FormLabel>
+                  </FormField>
+                  <FormField type="radio" className="radio-field">
+                    <FormLabel htmlFor="when-pigs-can-fly" type="radio">
+                      <RadioButton
+                        className="utrecht-form-field__input"
+                        id="when-pigs-can-fly"
+                        name="828aa90e-0e99-4db9-a5e5-d39f689cf7b3"
+                        value="3"
+                      />
+                      Wanneer varkens kunnen vliegen
+                    </FormLabel>
+                  </FormField>
+                </Fieldset>
+              </section>
+              <section className="voorbeeld-section--spacing">
+                <FormLabel>Contactgegevens:</FormLabel>
+                <Paragraph>
+                  Laat uw naam en e-mailadres achter, zodat we u op de hoogte kunnen houden van eventuele magische
+                  ontwikkelingen.
+                </Paragraph>
+                <FormField className="voorbeeld-formField" invalid={!!errors.fullName}>
+                  <FormLabel htmlFor="fullName">Volledige naam:</FormLabel>
+                  <Textbox
+                    invalid={!!errors.fullName}
+                    {...register('fullName', {
+                      required: 'Dit is verplicht',
+                      minLength: { value: 4, message: 'Min length is 4' },
+                    })}
+                    placeholder="Vul hier uw volledige naam in"
+                    id="fullName"
+                  />
+                </FormField>
+                <Paragraph>{errors.fullName?.message}</Paragraph>
+                <FormField className="voorbeeld-formField" invalid={!!errors.emailAdress}>
+                  <FormLabel htmlFor="emailAdress">E-mailadres:</FormLabel>
+                  <Textbox
+                    invalid={!!errors.emailAdress}
+                    {...register('emailAdress', { required: 'Dit is verplicht' })}
+                    placeholder="Vul hier uw e-mailadres in"
+                    id="emailAdress"
+                  />
+                </FormField>
+                <Paragraph>{errors.emailAdress?.message}</Paragraph>
+                <Button className="voorbeeld-button--spacing" type="submit" appearance="primary-action-button">
+                  Dien uw melding in
+                </Button>
+              </section>
             </form>
             <Paragraph>
               Dank u voor het deelnemen aan ons absurde meldpunt. Hoewel we geen garanties kunnen geven over de snelheid
