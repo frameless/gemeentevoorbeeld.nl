f6aafbd156fc978e36dcc74f0c387b0eec5c3309
ryanvvalkenhoef <76593083+ryanvvalkenhoef@users.noreply.github.com>
2024-01-30 18:09:40 +0100

    :art: Added section with Accordion

--- packages/next-templates/src/app/page.tsx
+++ packages/next-templates/src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Page, PageContent } from '@utrecht/component-library-react';
+import { AccordionProvider, Page, PageContent } from '@utrecht/component-library-react';
 import { TopTaskLink } from '@/components/TopTask/TopTaskLink';
 import { TopTaskNavigation } from '@/components/TopTask/TopTaskNavigation';
 import {
@@ -21,16 +21,41 @@ export default function Home() {
       <ExampleHeader />
       <ExampleNavigation />
       <PageContent className="voorbeeld-page-content--flex">
-        <TopTaskNavigation className="grid">
-          <TopTaskLink icon={<UtrechtIconPaspoort />}>Paspoort of ID-kaart aanvragen</TopTaskLink>
-          <TopTaskLink href="/meldingen/" icon={<UtrechtIconMeldingKlacht />}>
-            Meldingen openbare ruimte
-          </TopTaskLink>
-          <TopTaskLink icon={<UtrechtIconVerhuizen />}>Verhuizing doorgeven</TopTaskLink>
-          <TopTaskLink icon={<UtrechtIconWerken />}>Werken bij de gemeente</TopTaskLink>
-          <TopTaskLink icon={<UtrechtIconNummerbord />}>Parkeren: kentekenwijziging doorgeven</TopTaskLink>
-          <TopTaskLink icon={<UtrechtIconAfvalScheiden />}>Afval</TopTaskLink>
-        </TopTaskNavigation>
+        <section className="voorbeeld-section-page voorbeeld-section--spacing">
+          <TopTaskNavigation className="grid">
+            <TopTaskLink icon={<UtrechtIconPaspoort />}>Paspoort of ID-kaart aanvragen</TopTaskLink>
+            <TopTaskLink href="/meldingen/" icon={<UtrechtIconMeldingKlacht />}>
+              Meldingen openbare ruimte
+            </TopTaskLink>
+            <TopTaskLink icon={<UtrechtIconVerhuizen />}>Verhuizing doorgeven</TopTaskLink>
+            <TopTaskLink icon={<UtrechtIconWerken />}>Werken bij de gemeente</TopTaskLink>
+            <TopTaskLink icon={<UtrechtIconNummerbord />}>Parkeren: kentekenwijziging doorgeven</TopTaskLink>
+            <TopTaskLink icon={<UtrechtIconAfvalScheiden />}>Afval</TopTaskLink>
+          </TopTaskNavigation>
+        </section>
+        <section className="voorbeeld-section-page voorbeeld-section--spacing">
+          <div className="voorbeeld-section-content">
+            <AccordionProvider
+              sections={[
+                {
+                  body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore\n      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\n      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla\n      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est\n      laborum.',
+                  expanded: undefined,
+                  label: 'Paspoort, ID-kaart en rijbewijs',
+                },
+                {
+                  body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore\n      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\n      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla\n      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est\n      laborum.',
+                  expanded: undefined,
+                  label: 'Verhuizen',
+                },
+                {
+                  body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore\n      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\n      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla\n      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est\n      laborum.',
+                  expanded: undefined,
+                  label: 'Info over de gemeente',
+                },
+              ]}
+            />
+          </div>
+        </section>
       </PageContent>
       <ExampleFooter />
     </Page>
