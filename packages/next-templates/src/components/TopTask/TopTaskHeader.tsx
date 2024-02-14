e6aee5c0e1532f9b2be81a049df2fd56f2f413ee
ryanvvalkenhoef <76593083+ryanvvalkenhoef@users.noreply.github.com>
2024-02-14 18:01:42 +0100

    :art: Created concept for TopTaskHeader component

---
+++ packages/next-templates/src/components/TopTask/TopTaskHeader.tsx
@@ -0,0 +1,23 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2024 Gemeentevoorbeeld.nl
+ * Copyright (c) 2024 Ryan van Valkenhoef
+ */
+
+import clsx from 'clsx';
+import { HTMLAttributes, ForwardedRef, forwardRef, PropsWithChildren } from 'react';
+import './TopTaskHeader.css';
+
+export type DivProps = HTMLAttributes<HTMLDivElement>;
+
+export const TopTaskHeader = forwardRef(
+  ({ children, className, ...restProps }: PropsWithChildren<DivProps>, ref: ForwardedRef<HTMLDivElement>) => {
+    return (
+      <div ref={ref} className={clsx('example-toptask-header', `example-toptask-header--${className}`)} {...restProps}>
+        {children}
+      </div>
+    );
+  },
+);
+
+TopTaskHeader.displayName = 'TopTaskHeader';
