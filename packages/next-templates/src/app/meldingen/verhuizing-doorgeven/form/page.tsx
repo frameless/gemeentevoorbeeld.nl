8a2ddb48be22819827c4d26e377da0e99ef035dd
Marwaxhello <112861555+Marwaxhello@users.noreply.github.com>
2024-02-15 18:51:16 +0100

    :art: Added Fieldset, FormField en FormLabel to form-page

--- packages/next-templates/src/app/meldingen/verhuizing-doorgeven/form/page.tsx
+++ packages/next-templates/src/app/meldingen/verhuizing-doorgeven/form/page.tsx
@@ -4,11 +4,16 @@ import {
   Article,
   Button,
   Document,
+  Fieldset,
+  FieldsetLegend,
+  FormField,
+  FormLabel,
   Heading1,
   Heading2,
   Page,
   PageContent,
   Paragraph,
+  RadioButton,
 } from '@utrecht/component-library-react';
 import '@/app/styling/css/form-page.css';
 import BacklinkIcon from '@/app/styling/assets/backlink-icon.svg';
@@ -33,6 +38,42 @@ export default function Home() {
         <PageContent>
           <Article className="voorbeeld-article-space">
             <Heading1>Verhuizing doorgeven</Heading1>
+
+            <Heading2>Uw Gegevens</Heading2>
+            <Paragraph>Naam</Paragraph>
+            <Paragraph>Geboren</Paragraph>
+            <Paragraph>Adress</Paragraph>
+
+            <Heading2>Uw Situatie</Heading2>
+            <Fieldset id="df861ef1-844a-42df-8365-b54f59474fb8" role="radiogroup">
+              <FormField type="radio">
+                <RadioButton id="right-now" name="828aa90e-0e99-4db9-a5e5-d39f689cf7b3" value="1" />
+                <FormLabel htmlFor="right-now" type="radio">
+                  Ik ben eigenaar van de woning
+                </FormLabel>
+              </FormField>
+
+              <FormField type="radio">
+                <RadioButton id="shortly" name="828aa90e-0e99-4db9-a5e5-d39f689cf7b3" value="2" />
+                <FormLabel htmlFor="shortly" type="radio">
+                  Ik heb een huurcontract en ik huur het gehele woning
+                </FormLabel>
+              </FormField>
+
+              <FormField type="radio">
+                <RadioButton id="when-pigs-can-fly" name="828aa90e-0e99-4db9-a5e5-d39f689cf7b3" value="3" />
+                <FormLabel htmlFor="when-pigs-can-fly" type="radio">
+                  Ik heb een huurcontract en ik huur een deel van de woning
+                </FormLabel>
+              </FormField>
+
+              <FormField type="radio">
+                <RadioButton id="when-pigs-can-fly" name="828aa90e-0e99-4db9-a5e5-d39f689cf7b3" value="4" />
+                <FormLabel htmlFor="when-pigs-can-fly" type="radio">
+                  Ik ga bij iemand wonen
+                </FormLabel>
+              </FormField>
+            </Fieldset>
           </Article>
         </PageContent>
         <ExampleFooterFocus />
