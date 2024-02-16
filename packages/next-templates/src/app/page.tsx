82478aa266c7755e51037f0bf62a3b2c99e274be
ryanvvalkenhoef <76593083+ryanvvalkenhoef@users.noreply.github.com>
2024-02-16 12:08:25 +0100

    :bento: Imported IconCalendar in homepage

--- packages/next-templates/src/app/page.tsx
+++ packages/next-templates/src/app/page.tsx
@@ -12,8 +12,6 @@ import {
   Paragraph,
   Page,
   PageContent,
-  UnorderedList,
-  UnorderedListItem,
 } from '@utrecht/component-library-react';
 import { TopTaskLink } from '@/components/TopTask/TopTaskLink';
 import { TopTaskNavigation } from '@/components/TopTask/TopTaskNavigation';
