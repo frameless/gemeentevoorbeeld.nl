d7d7baf87cae58dc7ba6ebe9da9a0aca16179603
Tessa Viergever <112861180+TessaViergever@users.noreply.github.com>
2024-03-05 14:27:32 +0100

    :sparkles: Add productenpagina-klachtenformulier folder + page.tsx(with code from other page to test:works) to wmebv folder  and add productenpagina-klachtenformulier.css(still empty) to the css folder.

---
+++ packages/next-templates/src/app/wmebv/productenpagina-klachtenformulier/page.tsx
@@ -0,0 +1,103 @@
+'use client';
+
+import {
+  UtrechtArticle,
+  UtrechtButton,
+  UtrechtButtonGroup,
+  UtrechtDigidLogo,
+  UtrechtHeading1,
+  UtrechtHeading2,
+  UtrechtIcon,
+  UtrechtLink,
+  UtrechtPage,
+  UtrechtPageContent,
+  UtrechtParagraph,
+} from '@utrecht/web-component-library-react';
+import { ExampleHeaderFunnelWmebv } from '@/components/wmebv/Header/ExampleHeaderFunnelWmebv';
+import { ExampleFooterWmebv } from '@/components/wmebv/Footer/ExampleFooterWmebv';
+import { ExampleNavigation } from '@/components/ExampleNavigation/ExampleNavigation';
+import { Fieldset, FieldsetLegend, FormField, FormLabel, RadioButton } from '@utrecht/component-library-react';
+import { IconArrowLeft } from '@tabler/icons-react';
+import '@/app/styling/css/wmebv.css';
+import { useEffect } from 'react';
+
+export default function home() {
+  const stepLabel = 'Vraag aan de gemeente';
+  const websiteLabel = 'gemeente Voorbeeld';
+
+  useEffect(() => {
+    if (typeof document?.title === 'string') {
+      document.title = `${stepLabel} - ${websiteLabel}`;
+    }
+  }, []);
+
+  return (
+    <UtrechtPage>
+      <ExampleHeaderFunnelWmebv />
+      <UtrechtPageContent className="voorbeeld-page-content-flex">
+        <UtrechtArticle id="main" className="voorbeeld-article-space ">
+          <form method="POST" action="/api/wmebv/choose-form">
+            <UtrechtButtonGroup>
+              <UtrechtLink href="/wmebv/Intro">
+                <UtrechtIcon>
+                  <IconArrowLeft />
+                </UtrechtIcon>
+                Terug
+              </UtrechtLink>
+            </UtrechtButtonGroup>
+            <UtrechtHeading1>Vraag aan de gemeente</UtrechtHeading1>
+            <UtrechtHeading2>Inloggen</UtrechtHeading2>
+            <UtrechtParagraph className="paragraph_digid">
+              Dankzij uw DigiD kunt u overal makkelijk en veilig inloggen. Uw persoonlijke gegevens blijven goed
+              beschermd. Wanneer u inlogt worden uw persoonlijke gegevens automatisch ingevuld.
+            </UtrechtParagraph>
+            <UtrechtDigidLogo className="voorbeeld-digid-logo" aria-label="DigiD logo" role="img" />
+            <Fieldset id="df861ef1-844a-42df-8365-b54f59474fb8" role="radiogroup">
+              <FieldsetLegend className="fieldlabel_inloggen">Wilt u inloggen?</FieldsetLegend>
+              <FormField type="radio">
+                <UtrechtParagraph className="utrecht-form-field__label utrecht-form-field__label--radio">
+                  <FormLabel
+                    className="voorbeeld-radio-button-form-label"
+                    htmlFor="800b7f3f-5048-4dcc-8f23-d04fa6ca4199"
+                    type="radio"
+                  >
+                    <RadioButton
+                      className="utrecht-form-field__input"
+                      id="800b7f3f-5048-4dcc-8f23-d04fa6ca4199"
+                      name="loggedIn"
+                      value="true"
+                    />
+                    Ja
+                  </FormLabel>
+                </UtrechtParagraph>
+              </FormField>
+              <FormField type="radio">
+                <UtrechtParagraph className="utrecht-form-field__label utrecht-form-field__label--radio">
+                  <FormLabel
+                    className="voorbeeld-radio-button-form-label"
+                    htmlFor="fbc03e63-42c2-46e3-9acc-183fa64c6dcd"
+                    type="radio"
+                  >
+                    <RadioButton
+                      className="utrecht-form-field__input"
+                      id="fbc03e63-42c2-46e3-9acc-183fa64c6dcd"
+                      name="loggedIn"
+                      value="false"
+                    />
+                    Nee
+                  </FormLabel>
+                </UtrechtParagraph>
+              </FormField>
+            </Fieldset>
+            <UtrechtButtonGroup>
+              <UtrechtButton type="submit" appearance="primary-action-button">
+                Doorgaan
+              </UtrechtButton>
+            </UtrechtButtonGroup>
+          </form>
+        </UtrechtArticle>
+      </UtrechtPageContent>
+      <ExampleFooterWmebv />
+    </UtrechtPage>
+  );
+}
