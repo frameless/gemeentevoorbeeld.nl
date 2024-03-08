6e799490ab80fa03d532f27f9a10bf5a1791a210
Marwaxhello <112861555+Marwaxhello@users.noreply.github.com>
2024-03-08 13:44:51 +0100

    :tada: Beginning project with title and header

---
+++ packages/next-templates/src/app/wmebv/mijn-omgeving/page.tsx
@@ -0,0 +1,16 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Mijn Omgeving Inloggen',
+  description: '...',
+};
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Mijn Omgeving</h1>
+    </div>
+  );
+};
+
+export default Page;
