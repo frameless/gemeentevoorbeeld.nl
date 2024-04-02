158a026c418e141914d11b831500c292d21a7047
Marwaxhello <112861555+Marwaxhello@users.noreply.github.com>
2024-04-02 18:18:46 +0200

    :art: Imported ExampleHeader and Footer, styling, design tokens and breadcrum

--- packages/next-templates/src/app/mijn-omgeving/page.tsx
+++ packages/next-templates/src/app/mijn-omgeving/page.tsx
@@ -2,23 +2,34 @@
 
 import {
   Article,
+  BreadcrumbNavLink,
+  BreadcrumbNav,
+  BreadcrumbNavSeparator,
   Document,
   Heading1,
   Heading2,
   Heading3,
+  Link,
   Paragraph,
   Page,
   PageContent,
 } from '@utrecht/component-library-react';
-import '@/app/styling/css/detail-page.css';
-import '@utrecht/design-tokens/dist/index.css';
 import { ExampleHeaderFunnel } from '@/components/ExampleHeader/ExampleHeaderFunnel/ExampleHeaderFunnel';
 import { ExampleFooterFocus } from '@/components/ExampleFooter/ExampleFooterFocus/ExampleFooterFocus';
 import { ExampleNavigation } from '@/components/ExampleNavigation/ExampleNavigation';
 import { UtrechtDigidLogo, UtrechtIconArrow } from '@utrecht/web-component-library-react';
+import { ExampleHeaderFunnelWmebv } from '@/components/wmebv/Header/ExampleHeaderFunnelWmebv';
+
+import { ExampleHeader } from '@/components/ExampleHeader/ExampleHeader';
+import { ExampleFooter } from '@/components/ExampleFooter/ExampleFooter';
+
 import { useForm } from 'react-hook-form';
 import { useRouter } from 'next/navigation';
+
 import BacklinkIcon from '@/app/styling/assets/backlink-icon.svg';
+import IconChevronRight from '../../app/styling/assets/chevronRight.svg';
+import '@/app/styling/css/detail-page.css';
+import '@utrecht/design-tokens/dist/index.css';
 
 export default function Home() {
   const router = useRouter();
