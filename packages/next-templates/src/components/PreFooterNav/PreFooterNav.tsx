6b3b9b528a0fbad666f536576ecb4eb4026ed836
Tessa Viergever <112861180+TessaViergever@users.noreply.github.com>
2024-04-03 14:16:25 +0200

    ✨ add media query for responsiveness small screen  pre footer nav componen  

--- packages/next-templates/src/components/PreFooterNav/PreFooterNav.tsx
+++ packages/next-templates/src/components/PreFooterNav/PreFooterNav.tsx
@@ -4,12 +4,12 @@ import './pre-footer-nav.css';
 
 export const PreFooterNav = () => (
   <div className="voorbeeld-pre-footer">
-    <Link href="#">
+    <Link className="voorbeeld-pre-footer-link" href="#">
       {/* empty link - page doesnt exist yet */}
       <IconChevronRight />
       Wat vindt u van deze pagina
     </Link>
-    <Link href="#">
+    <Link className="voorbeeld-pre-footer-link" href="#">
       Naar boven
       <IconChevronUp />
     </Link>
