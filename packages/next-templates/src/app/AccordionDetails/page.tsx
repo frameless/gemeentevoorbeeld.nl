c334e3f9d3461863df75516a30d0762ee9f6e9e6
savitris <11764984+savitris@users.noreply.github.com>
2024-04-10 11:10:38 +0200

    💩 WIP

--- packages/next-templates/src/app/AccordionDetails/page.tsx
+++ packages/next-templates/src/app/AccordionDetails/page.tsx
@@ -1,6 +1,8 @@
-import { Heading3 } from '@utrecht/component-library-react';
+import { Heading3, Paragraph } from '@utrecht/component-library-react';
 import clsx from 'clsx';
+import { IconChevronDown } from '@tabler/icons-react';
 import { ForwardedRef, forwardRef, HTMLAttributes, KeyboardEvent, PropsWithChildren, useRef, useState } from 'react';
+import './index.scss';
 
 export default function home() {
   return (
@@ -8,10 +10,16 @@ export default function home() {
       <h2>Details/Summary</h2>
       <div>
         <details className="utrecht-accordion">
-          <summary className="utrecht-accordion__section">
-            <Heading3 className="utrecht-button utrecht-button--subtle utrecht-accordion__button"></Heading3>
+          <summary className="summary">
+            <Heading3 className="utrecht-button utrecht-button--subtle utrecht-accordion__button">Item 1</Heading3>
+            <div className="icon-chevron-down-wrapper">
+              <IconChevronDown />
+            </div>
           </summary>
-          <div className="utrecht-accordion__panel"></div>
+          <Paragraph className="utrecht-accordion__panel">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est aspernatur eos sed iusto vitae tenetur harum
+            consequatur modi corrupti ducimus unde odio, voluptate minima. Quam alias fugit adipisci facilis vitae.
+          </Paragraph>
         </details>
       </div>
     </div>
