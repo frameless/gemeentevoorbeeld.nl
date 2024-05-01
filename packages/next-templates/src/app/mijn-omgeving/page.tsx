63a4e7e612d668dabf5d30cbd222e5df3801685e
Marwaxhello <112861555+Marwaxhello@users.noreply.github.com>
2024-05-01 09:35:19 +0200

    :fire: Removed Navigation

--- packages/next-templates/src/app/mijn-omgeving/page.tsx
+++ packages/next-templates/src/app/mijn-omgeving/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
       <Page>
         {/* <ExampleHeader /> */}
         <HeaderIngelogd />
-        <ExampleNavigation />
+        {/* <ExampleNavigation /> */}
 
         <PageContent>
           <BreadcrumbNav label="Kruimelpad">
